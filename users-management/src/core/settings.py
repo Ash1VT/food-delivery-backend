9426f273c2c318618d8ed96fb917797d663880af
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-07-15 01:54:36 +0300

    added new installed apps and configs for rest_framework_simplejwt

--- users-management/src/core/settings.py
+++ users-management/src/core/settings.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from configurations import Configuration
 from pathlib import Path
 from .env import env
@@ -21,8 +23,11 @@ class Base(Configuration):
         'django.contrib.messages',
         'django.contrib.staticfiles',
         'phonenumber_field',
+        'rest_framework',
+        'rest_framework_simplejwt',
 
         'users',
+        'tokens',
     ]
 
     MIDDLEWARE = [
@@ -52,6 +57,61 @@ class Base(Configuration):
             },
         },
     ]
+
+    REST_FRAMEWORK = {
+        'DEFAULT_AUTHENTICATION_CLASSES': [
+            'tokens.authentication.CookieJWTAuthentication'
+        ]
+    }
+
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
+        "ROTATE_REFRESH_TOKENS": True,
+        "BLACKLIST_AFTER_ROTATION": True,
+        "UPDATE_LAST_LOGIN": False,
+
+        "ALGORITHM": "HS256",
+        "SIGNING_KEY": SECRET_KEY,
+        "VERIFYING_KEY": "",
+        "AUDIENCE": None,
+        "ISSUER": None,
+        "JSON_ENCODER": None,
+        "JWK_URL": None,
+        "LEEWAY": 0,
+
+        "AUTH_HEADER_TYPES": ("Bearer",),
+        "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+        "USER_ID_FIELD": "id",
+        "USER_ID_CLAIM": "user_id",
+        "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+        "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+        "TOKEN_TYPE_CLAIM": "token_type",
+        "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+        "JTI_CLAIM": "jti",
+
+        "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+        "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+        "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+
+        "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+        "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+        "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
+        "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+        "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
+        "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
+
+        'AUTH_COOKIE_ACCESS': 'access_token',  # Cookie name. Enables cookies if value is set.
+        'AUTH_COOKIE_REFRESH': 'refresh_token',  # Cookie name. Enables cookies if value is set.
+        'AUTH_COOKIE_DOMAIN': None,  # A string like "example.com", or None for standard domain cookie.
+        'AUTH_COOKIE_SECURE': False,  # Whether the auth cookies should be secure (https:// only).
+        'AUTH_COOKIE_HTTP_ONLY': True,  # Http only cookie flag.It's not fetch by javascript.
+        'AUTH_COOKIE_PATH': '/',  # The path of the auth cookie.
+        'AUTH_COOKIE_SAMESITE': 'Lax',  # Whether to set the flag restricting cookie leaks on cross-site requests.
+
+    }
 
     WSGI_APPLICATION = 'core.wsgi.application'
 
