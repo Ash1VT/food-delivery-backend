91fd96466f706a8facbd3b273809714d4420c4df
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-07-23 21:35:41 +0300

    added urls for new views

--- users-management/src/users/urls.py
+++ users-management/src/users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import RegisterCustomerView, RegisterCourierView
+from .views import CreateCustomerView, CreateCourierView, ListUsersView, RetrieveUpdateUserView, \
+    RetrieveUpdateCurrentUserView
 
 urlpatterns = [
-    path('customers/', RegisterCustomerView.as_view(), name='register_customer'),
-    path('couriers/', RegisterCourierView.as_view(), name='register_courier'),
+    path('', ListUsersView.as_view(), name='list_users'),
+    path('<int:pk>/', RetrieveUpdateUserView.as_view(), name='retrieve_update_user'),
+    path('customers/', CreateCustomerView.as_view(), name='register_customer'),
+    path('couriers/', CreateCourierView.as_view(), name='register_courier'),
+    path('current/', RetrieveUpdateCurrentUserView.as_view(), name='retrieve_update_current_user')
 ]
