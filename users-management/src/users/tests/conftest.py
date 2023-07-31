aca3bfd789af87101b44e3ce7af82732ea896aea
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-07-31 20:53:46 +0300

    added new fixtures for testing email verification

--- users-management/src/users/tests/conftest.py
+++ users-management/src/users/tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from django.test import Client
+from django.core import mail
 
 from users.managers import UserManager
 from users.models import User
@@ -20,8 +21,18 @@ def client_customer_with_all_tokens(customer: User):
 
 
 @pytest.fixture
+def client_customer_with_unverified_email(customer_with_unverified_email: User):
+    return create_client_with_all_tokens(customer_with_unverified_email)
+
+
+@pytest.fixture
 def client_moderator_with_all_tokens(moderator: User):
     return create_client_with_all_tokens(moderator)
+
+
+@pytest.fixture
+def client_moderator_with_unverified_email(moderator_with_unverified_email: User):
+    return create_client_with_all_tokens(moderator_with_unverified_email)
 
 
 # User manager fixtures #
@@ -36,11 +47,30 @@ def user_manager():
 # User models #
 
 @pytest.fixture
+def customer_with_unverified_email() -> User:
+    user_data = generate_valid_register_user_data()
+    user_profile_data = user_data.pop('user_profile')
+    user = UserService.create_customer(user_data=user_data, user_profile_data=user_profile_data)
+    mail.outbox.clear()
+    return user
+
+
+@pytest.fixture
+def moderator_with_unverified_email() -> User:
+    user_data = generate_valid_register_user_data()
+    user_profile_data = user_data.pop('user_profile')
+    user = UserService.create_moderator(user_data=user_data, user_profile_data=user_profile_data)
+    mail.outbox.clear()
+    return user
+
+
+@pytest.fixture
 def customer() -> User:
     user_data = generate_valid_register_user_data()
     user_profile_data = user_data.pop('user_profile')
     user = UserService.create_customer(user_data=user_data, user_profile_data=user_profile_data)
-    user.is_active = True
+    mail.outbox.clear()
+    user.is_email_verified = True
     user.save()
     return user
 
@@ -50,6 +80,7 @@ def moderator() -> User:
     user_data = generate_valid_register_user_data()
     user_profile_data = user_data.pop('user_profile')
     user = UserService.create_moderator(user_data=user_data, user_profile_data=user_profile_data)
-    user.is_active = True
+    mail.outbox.clear()
+    user.is_email_verified = True
     user.save()
     return user
