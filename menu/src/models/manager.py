27861447c3100dce5e04efa2f1748c520c2c9b43
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-08-28 18:47:25 +0300

    added manager model

---
+++ menu/src/models/manager.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+
+from .base import CustomBase
+
+
+class RestaurantManager(CustomBase):
+    __tablename__ = 'restaurant_managers'
+
+    id = Column(Integer, primary_key=True, autoincrement=False)
+
+    is_active = Column(Boolean, default=False)
+
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id', use_alter=True), unique=True, nullable=False)
+
+    restaurant = relationship("Restaurant", uselist=False)
