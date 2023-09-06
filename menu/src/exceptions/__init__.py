8a02449d2f73059c5381bc420837e181974b742e
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-09-06 21:40:52 +0300

    added exception when item is not in category

--- menu/src/exceptions/__init__.py
+++ menu/src/exceptions/__init__.py
@@ -1,6 +1,6 @@
 from .base import AppError, DatabaseInstanceNotFoundError, DatabaseInstanceAlreadyExistsError
 from .category import MenuCategoryNotFoundWithIdError
-from .item import MenuItemNotFoundWithIdError, MenuItemAlreadyInCategoryError
+from .item import MenuItemNotFoundWithIdError, MenuItemAlreadyInCategoryError, MenuItemNotInCategoryError
 from .menu import MenuNotFoundWithIdError, CurrentMenuMissingError
 from .restaurant import RestaurantNotFoundWithIdError, RestaurantAlreadyExistsWithIdError
 from .manager import RestaurantManagerNotFoundWithIdError, RestaurantManagerNotActiveError, \
--- menu/src/exceptions/item.py
+++ menu/src/exceptions/item.py
@@ -21,3 +21,18 @@ class MenuItemAlreadyInCategoryError(AppError):
     @property
     def message(self) -> str:
         return f"MenuItem is already in Category with id={self._category_id}"
+
+
+class MenuItemNotInCategoryError(AppError):
+
+    def __init__(self, category_id: int):
+        self._category_id = category_id
+        super().__init__()
+
+    @property
+    def status_code(self) -> int:
+        return 400
+
+    @property
+    def message(self) -> str:
+        return f"MenuItem is not found in Category with id={self._category_id}"
