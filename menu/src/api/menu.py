37d2292cefcb823b96eb211cd130c5ed4eaa5337
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-09-06 21:17:22 +0300

    added routes for menu

---
+++ menu/src/api/menu.py
@@ -0,0 +1,35 @@
+from typing import List
+
+from fastapi import APIRouter, Depends
+from models import RestaurantManager
+from schemas import RestaurantManagerCreateIn
+from uow import SqlAlchemyUnitOfWork
+from dependencies import get_uow, get_uow_with_commit, get_restaurant_manager
+from schemas.category import MenuCategoryRetrieveOut
+from schemas.menu import MenuRetrieveOut, MenuCreateIn, MenuCreateOut, MenuUpdateIn, MenuUpdateOut
+from services import MenuCategoryService, MenuService, RestaurantManagerService
+from decorators import handle_app_errors
+
+
+router = APIRouter(
+    prefix='/menus'
+)
+
+
+@router.post('/', response_model=MenuCreateOut)
+@handle_app_errors
+async def create_menu(menu: MenuCreateIn,
+                      restaurant_manager: RestaurantManager = Depends(get_restaurant_manager),
+                      uow: SqlAlchemyUnitOfWork = Depends(get_uow_with_commit)):
+    menu_service = MenuService(restaurant_manager)
+    return await menu_service.create(menu, uow)
+
+
+@router.put('/{menu_id}', response_model=MenuUpdateOut)
+@handle_app_errors
+async def update_menu(menu_id: int,
+                      menu: MenuUpdateIn,
+                      restaurant_manager: RestaurantManager = Depends(get_restaurant_manager),
+                      uow: SqlAlchemyUnitOfWork = Depends(get_uow_with_commit)):
+    menu_service = MenuService(restaurant_manager)
+    return await menu_service.update(menu_id, menu, uow)
