e137c8fc0e7898bfd4c740fc414b6a99a6be7011
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-09-06 21:14:46 +0300

    added util for checking restaurant manager ownership of menu

--- menu/src/utils/__init__.py
+++ menu/src/utils/__init__.py
@@ -1,2 +1,3 @@
-from .checks import check_restaurant_manager_is_active, check_restaurant_manager_ownership
+from .checks import check_restaurant_manager_is_active, check_restaurant_manager_ownership_on_restaurant, \
+    check_restaurant_manager_ownership_on_menu
 from .uow import uow_transaction, uow_transaction_with_commit
