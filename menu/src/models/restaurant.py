2c17ce13a21e7315e327e3efd9e132035b16e1af
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-09-06 21:11:12 +0300

    removed relationships

--- menu/src/models/restaurant.py
+++ menu/src/models/restaurant.py
@@ -10,8 +10,5 @@ class Restaurant(CustomBase):
     id = Column(Integer, primary_key=True, autoincrement=False)
     current_menu_id = Column(Integer, ForeignKey('menus.id', use_alter=True), unique=True)
 
-    current_menu = relationship("Menu", foreign_keys=[current_menu_id], uselist=False, post_update=True)
-    menus = relationship("Menu", foreign_keys="Menu.restaurant_id")
-
     def __str__(self):
         return str(self.id)
