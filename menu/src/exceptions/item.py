eccca8158bed0ca55e6977d4ccdf1a7b1cff4d79
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-09-06 21:04:32 +0300

    added new exceptions

--- menu/src/exceptions/item.py
+++ menu/src/exceptions/item.py
@@ -1,8 +1,23 @@
 from models import MenuItem
-from .base import DatabaseInstanceNotFoundError
+from .base import AppError, DatabaseInstanceNotFoundError
 
 
 class MenuItemNotFoundWithIdError(DatabaseInstanceNotFoundError):
 
     def __init__(self, id: int):
         super().__init__('id', id, MenuItem)
+
+
+class MenuItemAlreadyInCategoryError(AppError):
+
+    def __init__(self, category_id: int):
+        self._category_id = category_id
+        super().__init__()
+
+    @property
+    def status_code(self) -> int:
+        return 400
+
+    @property
+    def message(self) -> str:
+        return f"MenuItem is already in Category with id={self._category_id}"
--- menu/src/exceptions/menu.py
+++ menu/src/exceptions/menu.py
@@ -1,8 +1,23 @@
 from models import Menu
-from .base import DatabaseInstanceNotFoundError
+from .base import AppError, DatabaseInstanceNotFoundError
 
 
 class MenuNotFoundWithIdError(DatabaseInstanceNotFoundError):
 
     def __init__(self, id: int):
         super().__init__('id', id, Menu)
+
+
+class CurrentMenuMissingError(AppError):
+
+    def __init__(self, restaurant_id: int):
+        self._restaurant_id = restaurant_id
+        super().__init__()
+
+    @property
+    def status_code(self) -> int:
+        return 404
+
+    @property
+    def message(self) -> str:
+        return f"Restaurant with id={self._restaurant_id} hasn't got current menu"
