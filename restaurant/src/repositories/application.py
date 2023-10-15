7cd1e99a618f0005b3ad5f6263b9363d9382fc99
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-10-15 17:50:35 +0300

    added methods for listing application of types

--- restaurant/src/repositories/application.py
+++ restaurant/src/repositories/application.py
@@ -1,4 +1,6 @@
-from models import RestaurantApplication
+from sqlalchemy import select
+
+from models import RestaurantApplication, ApplicationType
 
 from .generic import SQLAlchemyRepository
 
@@ -9,3 +11,19 @@ class RestaurantApplicationRepository(SQLAlchemyRepository[RestaurantApplication
     """Repository for RestaurantApplication model operations."""
 
     model = RestaurantApplication
+
+    def _get_list_create_applications_stmt(self, **kwargs):
+        return select(RestaurantApplication).where(RestaurantApplication.application_type == ApplicationType.create)
+
+    def _get_list_update_applications_stmt(self, **kwargs):
+        return select(RestaurantApplication).where(RestaurantApplication.application_type == ApplicationType.update)
+
+    async def list_create_applications(self, **kwargs):
+        stmt = self._get_list_create_applications_stmt(**kwargs)
+        result = await self._session.execute(stmt)
+        return [r[0] for r in result.fetchall()]
+
+    async def list_update_applications(self, **kwargs):
+        stmt = self._get_list_update_applications_stmt(**kwargs)
+        result = await self._session.execute(stmt)
+        return [r[0] for r in result.fetchall()]
