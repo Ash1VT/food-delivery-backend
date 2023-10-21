0b5de9c80038288001021c76e088ca03e3f6b8ff
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-10-21 15:16:09 +0300

    replaced with new grpc authentication system

--- restaurant/src/grpc_files/__init__.py
+++ restaurant/src/grpc_files/__init__.py
@@ -1,7 +1,7 @@
 from config import get_settings
-from .permissions_client import *
+from .roles_client import *
 
 settings = get_settings()
 
-grpc_permissions_client = PermissionsClient(host=settings.permissions_grpc_server_host,
-                                            port=settings.permissions_grpc_server_port)
+grpc_roles_client = RolesClient(host=settings.permissions_grpc_server_host,
+                                port=settings.permissions_grpc_server_port)
---
+++ restaurant/src/grpc_files/roles_client.py
@@ -0,0 +1,26 @@
+import grpc
+import grpc_files.generated.roles_pb2_grpc as pb2_grpc
+import grpc_files.generated.roles_pb2 as pb2
+
+__all__ = ["RolesClient"]
+
+
+class RolesClient(object):
+    """
+    Client for gRPC functionality
+    """
+
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.server_port = port
+
+        # instantiate a channel
+        self.channel = grpc.insecure_channel(
+            '{}:{}'.format(self.host, self.server_port))
+
+        # bind the client
+        self.stub = pb2_grpc.RolesStub(self.channel)
+
+    def get_user_role(self, access_token):
+        request = pb2.GetUserRoleRequest(access_token=access_token)
+        return self.stub.GetUserRole(request)
