440b400184d04840d57c640cf5cac203b074f53b
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-10-22 23:01:37 +0300

    removed 'is_active' field from admin panel

--- menu/src/setup/admin/manager.py
+++ menu/src/setup/admin/manager.py
@@ -4,7 +4,6 @@ from models import RestaurantManager
 
 class RestaurantManagerAdmin(ModelView, model=RestaurantManager):
     column_list = [RestaurantManager.id,
-                   RestaurantManager.is_active,
                    RestaurantManager.restaurant_id]
 
     icon = "fa-solid fa-user"
