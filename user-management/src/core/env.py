ad02927645b531d2ee72af411e6aa0f1bd6e63f9
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-10-28 20:42:55 +0300

    changed envs initialization

--- user-management/src/core/env.py
+++ user-management/src/core/env.py
@@ -2,8 +2,13 @@ from pathlib import Path
 from environ import Env
 
 # Env file path
-ENV_FILE = Path(__file__).resolve().parent.parent.parent / '.env'
+env = Env()
+
+configuration = env('DJANGO_CONFIGURATION')
+ENV_DIRECTORY = Path(__file__).resolve().parent.parent.parent / 'env'
 
 # Initialise environment variables
-env = Env()
-Env.read_env(env_file=ENV_FILE)
+if configuration == 'Develop':
+    Env.read_env(env_file=ENV_DIRECTORY / '.env.dev')
+elif configuration == 'Test':
+    Env.read_env(env_file=ENV_DIRECTORY / '.env.test')
