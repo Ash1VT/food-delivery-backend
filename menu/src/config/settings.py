bc2fe48c5715fd02a523dfbdb17fe7495e1c9a67
Панасевич Никита <nikita.panasevitch@yandex.by>
2023-10-31 14:58:08 +0300

    added kafka settings

--- menu/src/config/settings.py
+++ menu/src/config/settings.py
@@ -1,4 +1,5 @@
 import os
+from typing import List, Dict
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from .directories import ENV_DIRECTORY, BASE_DIRECTORY, SRC_DIRECTORY
@@ -11,6 +12,23 @@ class Settings(BaseSettings):
     reload: bool
     roles_grpc_server_host: str
     roles_grpc_server_port: str
+    kafka_bootstrap_server_host: str
+    kafka_bootstrap_server_port: int
+    kafka_sasl_mechanism: str = 'PLAIN'
+    kafka_broker_user: str
+    kafka_broker_password: str
+
+    kafka_group_consumers_count: int = 1
+    kafka_consumer_topic_events: Dict[str, List[str]] = {
+        'menu_restaurant': [
+            'RestaurantApplicationConfirmedEvent',
+            'RestaurantActivatedEvent',
+            'RestaurantDeactivatedEvent',
+        ],
+        'user_menu': [
+            'RestaurantManagerCreatedEvent',
+        ]
+    }
 
 
 class DevelopSettings(Settings):
@@ -21,20 +39,14 @@ class DevelopSettings(Settings):
     pg_user: str
     pg_password: str
 
-    model_config = SettingsConfigDict(env_file=[ENV_DIRECTORY / '.env.dev',
-                                                ENV_DIRECTORY / '.env',
-                                                SRC_DIRECTORY / '.env',
-                                                BASE_DIRECTORY / '.env'])
+    model_config = SettingsConfigDict(env_file=ENV_DIRECTORY / '.env.dev')
 
 
 class TestSettings(Settings):
     reload: bool = False
     sqlite_db_file: str
 
-    model_config = SettingsConfigDict(env_file=[ENV_DIRECTORY / '.env.test',
-                                                ENV_DIRECTORY / '.env',
-                                                SRC_DIRECTORY / '.env',
-                                                BASE_DIRECTORY / '.env'])
+    model_config = SettingsConfigDict(env_file=ENV_DIRECTORY / '.env.test')
 
 
 @config_cache
