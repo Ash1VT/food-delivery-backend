7ffe0392c8fb3d438c64f5408ca49541aaa519f7
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-10 22:16:01 +0300

    added types for config files

---
+++ order/src/types/config.type.ts
@@ -0,0 +1,10 @@
+export type ConfiguredFunction = (...args: any[]) => any;
+
+export interface SettingsOptions {
+    pgHost: string
+    pgPort: string
+    pgDatabase: string
+    pgUser: string
+    pgPassword: string
+    databaseURL: string
+}
