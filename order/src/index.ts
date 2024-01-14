0a8ff97ed706ed6acffc72e0d496d0e3495a668c
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-15 01:25:54 +0300

    modified in test purposes

--- order/src/index.ts
+++ order/src/index.ts
@@ -2,12 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import getSettings from "./config";
 import { PrismaClient } from '@prisma/client'
+import OrderRepository from "./repositories/OrderRepository";
+import CustomerRepository from "./repositories/CustomerRepository";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: Unreachable code error
+BigInt.prototype.toJSON = function () {
+    return this.toString();
+};
 
 const app = express()
 const settings = getSettings()
-
-app.get('/', (req, res) => {
-    res.json({'message': settings.variables})
+const prismaClient = new PrismaClient()
+const orderRepository = new OrderRepository(prismaClient)
+const customerRepository = new CustomerRepository(prismaClient)
+app.get('/', async (req, res) => {
+    res.json({'message': await customerRepository.getMany()})
 })
 
 // app.get('/hi', (req, res) => {
