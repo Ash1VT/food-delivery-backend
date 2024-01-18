d65ac2552404b5f52f57d3fb035e27cc0bf4de9d
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-18 17:49:18 +0300

    added dto models for order items

---
+++ order/src/dto/orderItem.ts
@@ -0,0 +1,15 @@
+interface OrderItemBaseDTO {
+    menuItemId: number
+    orderId: number
+    quantity: number
+}
+
+interface OrderItemBaseOutputDTO extends OrderItemBaseDTO {
+    id: number
+}
+
+export interface OrderItemGetOutputDTO extends OrderItemBaseOutputDTO {}
+
+export interface OrderItemCreateInputDTO extends OrderItemBaseDTO {}
+
+export interface OrderItemCreateOutputDTO extends OrderItemBaseOutputDTO {}
