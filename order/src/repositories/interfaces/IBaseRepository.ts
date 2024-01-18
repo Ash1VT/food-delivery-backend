168248e471f3913956e5b970f087d2f2b4500146
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-19 01:12:45 +0300

    concretized template names

---
+++ order/src/repositories/interfaces/IBaseRepository.ts
@@ -0,0 +1,7 @@
+export default interface IBaseRepository<Model, CreateInput, UpdateInput> {
+    getOne(id: number): Promise<Model | null>
+    getMany(): Promise<Model[]>
+    create(data: CreateInput): Promise<Model>
+    update(id: number, data: UpdateInput): Promise<Model | null>
+    delete(id: number): any
+}
