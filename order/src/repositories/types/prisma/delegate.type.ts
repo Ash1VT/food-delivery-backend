099b2460d07f5524d4db91b7ee4b136504461b6d
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-19 01:17:42 +0300

    moved prisma delegates to separate file and folder

---
+++ order/src/repositories/types/prisma/delegate.type.ts
@@ -0,0 +1,12 @@
+import { Prisma } from "@prisma/client";
+
+export type CourierDelegate = Prisma.CourierDelegate
+export type CustomerDelegate = Prisma.CustomerDelegate
+export type MenuItemDelegate = Prisma.MenuItemDelegate
+export type ModeratorDelegate = Prisma.ModeratorDelegate
+export type OrderDelegate = Prisma.OrderDelegate
+export type OrderItemDelegate = Prisma.OrderItemDelegate
+export type PromocodeDelegate = Prisma.PromocodeDelegate
+export type PromotionDelegate = Prisma.PromotionDelegate
+export type RestaurantDelegate = Prisma.RestaurantDelegate
+export type RestaurantManagerDelegate = Prisma.RestaurantManagerDelegate
