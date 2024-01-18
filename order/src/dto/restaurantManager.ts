9ecf83c004dce4cf6c01fef74081a52d5ff60efc
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-18 17:48:09 +0300

    added dto models for restaurant manager

---
+++ order/src/dto/restaurantManager.ts
@@ -0,0 +1,13 @@
+interface RestaurantManagerBaseDTO {
+    id: number
+}
+
+interface RestaurantManagerBaseOutputDTO extends RestaurantManagerBaseDTO {
+    restaurantId?: number
+}
+
+export interface RestaurantManagerGetOutputDTO extends RestaurantManagerBaseOutputDTO {}
+
+export interface RestaurantManagerCreateInputDTO extends RestaurantManagerBaseDTO {}
+
+export interface RestaurantManagerCreateOutputDTO extends RestaurantManagerBaseOutputDTO {}
