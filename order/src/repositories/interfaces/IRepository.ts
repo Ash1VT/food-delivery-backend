a807c981f8e324d509811f9ed74a778f1e164c1a
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-19 01:20:52 +0300

    removed IRepository interface

--- order/src/repositories/interfaces/IRepository.ts
+++
@@ -1,7 +0,0 @@
-export default interface IRepository<T, K, M> {
-    getOne(id: number): Promise<T | null>
-    getMany(): Promise<T[]>
-    create(data: K): Promise<T>
-    update(id: number, data: M): Promise<T | null>
-    delete(id: number): any
-}
