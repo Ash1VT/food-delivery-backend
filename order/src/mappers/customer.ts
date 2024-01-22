81d4fdf9046d0359870c5d9c7c2ddf4ecc604f78
ash1vt <nikita.panasevitch@yandex.by>
2024-01-22 22:35:02 +0300

    finished with mappers for customer

--- order/src/mappers/customer.ts
+++ order/src/mappers/customer.ts
@@ -1,44 +1,44 @@
 import { CustomerCreateInput, CustomerModel } from "../models/customer";
 import { CustomerGetOutputDTO, CustomerCreateOutputDTO, CustomerCreateInputDTO } from "../dto/customer";
 import { ICustomerCreateMapper, ICustomerGetMapper } from "./interfaces/instances/customer";
-import { CustomerCreateAdditionalData } from "./types/additionalData";
+import { CustomerCreateDbModelAdditionalData, CustomerCreateDtoModelAdditionalData, CustomerGetDtoModelAdditionalData } from "./types/additionalData";
+import toDtos from "./utils";
+import mapManyModels from "./utils";
 
 export class CustomerGetMapper implements ICustomerGetMapper {
 
-    toDto(dbModel: CustomerModel): CustomerGetOutputDTO {
+    toDto(dbModel: CustomerModel, additionalData: CustomerGetDtoModelAdditionalData): CustomerGetOutputDTO {
         return {
             id: Number(dbModel.id)
         }
     }
 
-    toDtos(dbModels: CustomerModel[]): CustomerGetOutputDTO[] {
-        return dbModels.map((dbModel) => this.toDto(dbModel))
+    toDtos(dbModels: CustomerModel[], additionalData: CustomerGetDtoModelAdditionalData[]): CustomerGetOutputDTO[] {
+        return mapManyModels(dbModels, this.toDto, additionalData)
     }
 
 }
 
 export class CustomerCreateMapper implements ICustomerCreateMapper {
 
-    toDto(dbModel: CustomerModel): CustomerCreateOutputDTO {
-        throw new Error("Method not implemented.");
+    toDto(dbModel: CustomerModel, additionalData: CustomerCreateDtoModelAdditionalData): CustomerCreateOutputDTO {
+        return {
+            id: Number(dbModel.id)
+        }
     }
 
-    toDtos(dbModels: CustomerModel[]): CustomerCreateOutputDTO[] {
-        throw new Error("Method not implemented.");
-    }
+    toDtos(dbModels: CustomerModel[], additionalData: CustomerCreateDtoModelAdditionalData[]): CustomerCreateOutputDTO[] {
+        return mapManyModels(dbModels, this.toDto, additionalData)
+    }   
 
-    toDbModel(dtoModel: CustomerCreateInputDTO, additionalData: CustomerCreateAdditionalData): CustomerCreateInput {
-        throw new Error("Method not implemented.");
+    toDbModel(dtoModel: CustomerCreateInputDTO, additionalData: CustomerCreateDbModelAdditionalData): CustomerCreateInput {
+        return {
+            id: BigInt(dtoModel.id)
+        }
     }
     
-    toDbModels(dtoModels: CustomerCreateInputDTO[], additionalData: CustomerCreateAdditionalData[]): CustomerCreateInput[] {
-        throw new Error("Method not implemented.");
+    toDbModels(dtoModels: CustomerCreateInputDTO[], additionalData: CustomerCreateDbModelAdditionalData[]): CustomerCreateInput[] {
+        return mapManyModels(dtoModels, this.toDbModel, additionalData)
     }
-
-
-    parse(data: any): CustomerCreateInputDTO {
-        throw new Error("Method not implemented.");
-    }
-
 }
 
