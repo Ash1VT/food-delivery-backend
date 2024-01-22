ef59aa0cae4eff5f2cff9701e959c3f33f2e23e2
ash1vt <nikita.panasevitch@yandex.by>
2024-01-22 22:28:51 +0300

    added additional data schemas

--- order/src/mappers/types/additionalData.ts
+++ order/src/mappers/types/additionalData.ts
@@ -1,47 +1,85 @@
-export type CourierCreateAdditionalData = {
+export type CourierCreateDbModelAdditionalData = {}
 
-}
+export type CourierCreateDtoModelAdditionalData = {}
 
-export type CustomerCreateAdditionalData = {
-    
-}
+export type CourierGetDtoModelAdditionalData = {}
 
-export type RestaurantManagerCreateAdditionalData = {
-    
-}
 
-export type ModeratorCreateAdditionalData = {
-    
-}
+export type CustomerCreateDbModelAdditionalData = {}
 
-export type PromocodeCreateAdditionalData = {
-    
-}
+export type CustomerCreateDtoModelAdditionalData = {}
 
-export type PromotionCreateAdditionalData = {
-    
-}
+export type CustomerGetDtoModelAdditionalData = {}
 
-export type OrderItemCreateAdditionalData = {
-    
-}
 
-export type MenuItemCreateAdditionalData = {
-    
-}
+export type RestaurantManagerCreateDbModelAdditionalData = {}
 
-export type MenuItemUpdateAdditionalData = {
-    
-}
+export type RestaurantManagerCreateDtoModelAdditionalData = {}
 
-export type OrderCreateAdditionalData = {
+export type RestaurantManagerGetDtoModelAdditionalData = {}
+
+
+export type ModeratorCreateDbModelAdditionalData = {}
+
+export type ModeratorCreateDtoModelAdditionalData = {}
+
+export type ModeratorGetDtoModelAdditionalData = {}
+
+
+export type PromocodeCreateDbModelAdditionalData = {}
+
+export type PromocodeCreateDtoModelAdditionalData = {}
+
+export type PromocodeGetDtoModelAdditionalData = {}
+
+
+export type PromotionCreateDbModelAdditionalData = {}
+
+export type PromotionCreateDtoModelAdditionalData = {}
+
+export type PromotionGetDtoModelAdditionalData = {}
+
+
+export type OrderItemCreateDbModelAdditionalData = {}
+
+export type OrderItemCreateDtoModelAdditionalData = {}
+
+export type OrderItemGetDtoModelAdditionalData = {}
+
+
+export type MenuItemCreateDbModelAdditionalData = {}
+
+export type MenuItemCreateDtoModelAdditionalData = {}
+
+export type MenuItemUpdateDbModelAdditionalData = {}
+
+export type MenuItemUpdateDtoModelAdditionalData = {}
+
+export type MenuItemGetDtoModelAdditionalData = {}
+
+
+export type OrderCreateDbModelAdditionalData = {
     customerId: number,
     promocodeId?: number,
     supposedDeliveryTime: Date,
     totalPrice: number,
     decountedPrice: number
+    itemsAdditionalData: OrderItemCreateDbModelAdditionalData[]
 }
 
-export type RestaurantCreateAdditionalData = {
-    
+export type OrderCreateDtoModelAdditionalData = {
+    promocode?: string
+    itemsAdditionalData: OrderItemCreateDtoModelAdditionalData[]
 }
+
+export type OrderGetDtoModelAdditionalData = {
+    promocode?: string
+    itemsAdditionalData: OrderItemGetDtoModelAdditionalData[]
+}
+
+
+export type RestaurantCreateDbModelAdditionalData = {}
+
+export type RestaurantCreateDtoModelAdditionalData = {}
+
+export type RestaurantGetDtoModelAdditionalData = {}
