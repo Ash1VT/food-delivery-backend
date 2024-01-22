0314f271ac1363b4dca4f4060d3b99a625b8e6dc
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-22 12:29:15 +0300

    added types for additional mapper data

---
+++ order/src/mappers/types/additionalData.ts
@@ -0,0 +1,47 @@
+export type CourierCreateAdditionalData = {
+
+}
+
+export type CustomerCreateAdditionalData = {
+    
+}
+
+export type RestaurantManagerCreateAdditionalData = {
+    
+}
+
+export type ModeratorCreateAdditionalData = {
+    
+}
+
+export type PromocodeCreateAdditionalData = {
+    
+}
+
+export type PromotionCreateAdditionalData = {
+    
+}
+
+export type OrderItemCreateAdditionalData = {
+    
+}
+
+export type MenuItemCreateAdditionalData = {
+    
+}
+
+export type MenuItemUpdateAdditionalData = {
+    
+}
+
+export type OrderCreateAdditionalData = {
+    customerId: number,
+    promocodeId?: number,
+    supposedDeliveryTime: Date,
+    totalPrice: number,
+    decountedPrice: number
+}
+
+export type RestaurantCreateAdditionalData = {
+    
+}
