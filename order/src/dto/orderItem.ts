ad85d3b852cc7e542c7ccc5b11a691f63e72ae79
ash1vt <nikita.panasevitch@yandex.by>
2024-01-22 22:25:07 +0300

    added order item create dtos with order

--- order/src/dto/orderItem.ts
+++ order/src/dto/orderItem.ts
@@ -7,8 +7,18 @@ interface OrderItemBaseOutputDTO extends OrderItemBaseDTO {
     id: number
 }
 
-export interface OrderItemGetOutputDTO extends OrderItemBaseOutputDTO {}
+export interface OrderItemGetOutputDTO extends OrderItemBaseOutputDTO {
+    orderId: number
+}
 
-export interface OrderItemCreateInputDTO extends OrderItemBaseDTO {}
+export interface OrderItemCreateInputDTO extends OrderItemBaseDTO {
+    orderId: number
+}
 
-export interface OrderItemCreateOutputDTO extends OrderItemBaseOutputDTO {}
+export interface OrderItemCreateOutputDTO extends OrderItemBaseOutputDTO {
+    orderId: number
+}
+
+export interface OrderItemWithOrderCreateInputDTO extends OrderItemBaseDTO {}
+
+export interface OrderItemWithOrderCreateOutputDTO extends OrderItemBaseOutputDTO {}
