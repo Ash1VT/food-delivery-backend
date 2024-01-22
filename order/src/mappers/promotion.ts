58ada61c8c7a5e02fd93b41d1cc3f62c71faf6c3
ash1vt <nikita.panasevitch@yandex.by>
2024-01-22 22:36:49 +0300

    finished with mappers for promotion

--- order/src/mappers/promotion.ts
+++ order/src/mappers/promotion.ts
@@ -1,41 +1,43 @@
 import { PromotionGetOutputDTO, PromotionCreateOutputDTO, PromotionCreateInputDTO } from "../dto/promotion";
 import { PromotionModel } from "../models/promotion";
 import { IPromotionGetMapper, IPromotionCreateMapper } from "./interfaces/instances/promotion";
+import { PromotionCreateDbModelAdditionalData, PromotionCreateDtoModelAdditionalData, PromotionGetDtoModelAdditionalData } from "./types/additionalData";
+import mapManyModels from "./utils";
 
 export class PromotionGetMapper implements IPromotionGetMapper {
 
-    toDto(dbModel: PromotionModel): PromotionGetOutputDTO {
+    toDto(dbModel: PromotionModel, additionalData: PromotionGetDtoModelAdditionalData): PromotionGetOutputDTO {
         return {
             id: Number(dbModel.id)
         }
     }
 
-    toDtos(dbModels: PromotionModel[]): PromotionGetOutputDTO[] {
-        return dbModels.map((dbModel) => this.toDto(dbModel))
+    toDtos(dbModels: PromotionModel[], additionalData: PromotionGetDtoModelAdditionalData[]): PromotionGetOutputDTO[] {
+        return mapManyModels(dbModels, this.toDto, additionalData)
     }
 
 }
 
 export class PromotionCreateMapper implements IPromotionCreateMapper {
 
-    toDto(dbModel: PromotionModel): PromotionCreateOutputDTO {
-        throw new Error("Method not implemented.");
+    toDto(dbModel: PromotionModel, additionalData: PromotionCreateDtoModelAdditionalData): PromotionCreateOutputDTO {
+        return {
+            id: Number(dbModel.id)
+        }
     }
 
-    toDtos(dbModels: PromotionModel[]): PromotionCreateOutputDTO[] {
-        throw new Error("Method not implemented.");
+    toDtos(dbModels: PromotionModel[], additionalData: PromotionCreateDtoModelAdditionalData[]): PromotionCreateOutputDTO[] {
+        return mapManyModels(dbModels, this.toDto, additionalData)
     }
     
-    toDbModel(dtoModel: PromotionCreateInputDTO): PromotionModel {
-        throw new Error("Method not implemented.");
+    toDbModel(dtoModel: PromotionCreateInputDTO, additionalData: PromotionCreateDbModelAdditionalData): PromotionModel {
+        return {
+            id: BigInt(dtoModel.id)
+        }
     }
 
-    toDbModels(dtoModels: PromotionCreateInputDTO[]): PromotionModel[] {
-        throw new Error("Method not implemented.");
-    }
-
-    parse(data: any): PromotionCreateInputDTO {
-        throw new Error("Method not implemented.");
+    toDbModels(dtoModels: PromotionCreateInputDTO[], additionalData: PromotionCreateDbModelAdditionalData[]): PromotionModel[] {
+        return mapManyModels(dtoModels, this.toDbModel, additionalData)
     }
 
 }
