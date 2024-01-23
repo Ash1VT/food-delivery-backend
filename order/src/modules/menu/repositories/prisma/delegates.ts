82d8aa4c438b1a0800f83599cf223c3cb7c6a19c
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-23 19:04:23 +0300

    divided prisma delegates into separate modules

---
+++ order/src/modules/menu/repositories/prisma/delegates.ts
@@ -0,0 +1,3 @@
+import { Prisma } from "@prisma/client";
+
+export type MenuItemDelegate = Prisma.MenuItemDelegate
---
+++ order/src/modules/promotions/repositories/prisma/delegates.ts
@@ -0,0 +1,4 @@
+import { Prisma } from "@prisma/client"
+
+export type PromocodeDelegate = Prisma.PromocodeDelegate
+export type PromotionDelegate = Prisma.PromotionDelegate
