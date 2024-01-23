4086281b228242326c0d0b8261e35e5386afeb42
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-23 19:05:42 +0300

    moved restaurant prisma repository to separate module

--- order/src/modules/restaurants/repositories/prisma/PrismaRestaurantRepository.ts
+++ order/src/modules/restaurants/repositories/prisma/PrismaRestaurantRepository.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-import PrismaBaseRepository from "./PrismaBaseRepository";
+import PrismaBaseRepository from "../../../../base/repositories/PrismaBaseRepository";
 import { RestaurantModel, RestaurantCreateInput, RestaurantUpdateInput } from "../../models/restaurant";
 import IRestaurantRepository from "../interfaces/IRestaurantRepository";
-import { RestaurantDelegate } from "../types/prisma/delegate.type";
+import { RestaurantDelegate } from "../delegates";
 
 
 export default class PrismaRestaurantRepository extends PrismaBaseRepository<RestaurantDelegate, RestaurantModel, RestaurantCreateInput, RestaurantUpdateInput>
