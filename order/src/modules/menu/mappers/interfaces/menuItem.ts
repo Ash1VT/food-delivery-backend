7380263f288596178d59c92d5cf419e874df07ad
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-24 01:04:16 +0300

    moved menu item mapper interfaces to separate module

--- order/src/modules/menu/mappers/interfaces/menuItem.ts
+++ order/src/modules/menu/mappers/interfaces/menuItem.ts
@@ -1,8 +1,8 @@
-import { MenuItemCreateOutputDTO, MenuItemCreateInputDTO, MenuItemUpdateOutputDTO, MenuItemUpdateInputDTO, MenuItemGetOutputDTO } from "../../../dto/menuItem";
-import { MenuItemCreateInput, MenuItemModel, MenuItemUpdateInput } from "../../../models/menuItem";
-import { MenuItemCreateDbModelAdditionalData, MenuItemCreateDtoModelAdditionalData, MenuItemGetDtoModelAdditionalData, MenuItemUpdateDbModelAdditionalData, MenuItemUpdateDtoModelAdditionalData } from "../../types/additionalData";
-import DatabaseToDtoMapper from "../IDatabaseToDtoMapper";
-import DtoToDatabaseMapper from "../IDtoToDatabaseMapper";
+import { MenuItemCreateOutputDTO, MenuItemCreateInputDTO, MenuItemUpdateOutputDTO, MenuItemUpdateInputDTO, MenuItemGetOutputDTO } from "../../dto/menuItem";
+import { MenuItemCreateInput, MenuItemModel, MenuItemUpdateInput } from "../../models/menuItem";
+import { MenuItemCreateDbModelAdditionalData, MenuItemCreateDtoModelAdditionalData, MenuItemGetDtoModelAdditionalData, MenuItemUpdateDbModelAdditionalData, MenuItemUpdateDtoModelAdditionalData } from "../../../../mappers/types/additionalData";
+import DatabaseToDtoMapper from "../../../../base/mappers/interfaces/IDatabaseToDtoMapper";
+import DtoToDatabaseMapper from "../../../../base/mappers/interfaces/IDtoToDatabaseMapper";
 
 
 export interface IMenuItemCreateMapper extends DatabaseToDtoMapper<MenuItemModel, MenuItemCreateOutputDTO, MenuItemCreateDtoModelAdditionalData>,
