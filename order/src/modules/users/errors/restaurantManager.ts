343462a4ac014d3d66014adaaa5f37b585eba4b1
ash1vt <nikita.panasevitch@yandex.by>
2024-01-25 17:29:23 +0300

    added restaurant manager ownership error

--- order/src/modules/users/errors/restaurantManager.ts
+++ order/src/modules/users/errors/restaurantManager.ts
@@ -1,3 +1,4 @@
+import AppError from "@src/base/errors/AppError";
 import DatabaseInstanceAlreadyExistsError from "@src/base/errors/DatabaseInstanceAlreadyExistsError";
 import DatabaseInstanceNotFoundError from "@src/base/errors/DatabaseInstanceNotFoundError";
 
@@ -13,6 +14,19 @@ export class RestaurantManagerAlreadyExistsWithIdError extends DatabaseInstanceA
 
     constructor(id: number) {
         super("id", id.toString(), "RestaurantManager")
+    }
+
+}
+
+export class RestaurantManagerOwnershipError extends AppError {
+    
+    constructor(restaurantManagerId: number, restaurantId: number) {
+        super(`Restaurant Manager with id=${restaurantManagerId} doesn't own
+               Restaurant with id=${restaurantId} to perform this action`)
+    }
+
+    public get statusCode(): number {
+        return 403
     }
 
 }
