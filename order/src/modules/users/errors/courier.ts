fa3e6b1d4efcf7dbdf58bd08ddf1e0935fdacf86
ash1vt <nikita.panasevitch@yandex.by>
2024-01-25 17:28:13 +0300

    added courier ownership error

--- order/src/modules/users/errors/courier.ts
+++ order/src/modules/users/errors/courier.ts
@@ -1,3 +1,4 @@
+import AppError from "@src/base/errors/AppError";
 import DatabaseInstanceAlreadyExistsError from "@src/base/errors/DatabaseInstanceAlreadyExistsError";
 import DatabaseInstanceNotFoundError from "@src/base/errors/DatabaseInstanceNotFoundError";
 
@@ -5,7 +6,7 @@ export class CourierNotFoundWithIdError extends DatabaseInstanceNotFoundError {
 
     constructor(id: number) {
         super("id", id.toString(), "Courier")
-    }
+    }   
 
 }
 
@@ -15,4 +16,28 @@ export class CourierAlreadyExistsWithIdError extends DatabaseInstanceAlreadyExis
         super("id", id.toString(), "Courier")
     }
 
+}
+
+export class CourierOwnershipError extends AppError {
+    
+    constructor(courierId: number, orderId: number) {
+        super(`Courier with id=${courierId} is not delivering Order with id=${orderId}`)
+    }
+
+    public get statusCode(): number {
+        return 403
+    }
+
+}
+
+export class CourierDeliveryLimitError extends AppError {
+    
+    constructor(courierId: number, orderLimit: number) {
+        super(`Courier with id=${courierId} reached limit of simultaneously delivering orders: ${orderLimit}`)
+    }
+
+    public get statusCode(): number {
+        return 403
+    }
+    
 }
