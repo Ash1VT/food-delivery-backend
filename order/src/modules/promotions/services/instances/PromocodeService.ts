453d8fee4cdc3d0dba966b147278e3b829981401
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-28 03:26:32 +0300

    added promotion services

---
+++ order/src/modules/promotions/services/instances/PromocodeService.ts
@@ -0,0 +1,98 @@
+import { PromocodeGetOutputDTO, PromocodeCreateInputDTO, PromocodeCreateOutputDTO, PromocodeUpdateInputDTO, PromocodeUpdateOutputDTO } from "../../dto/promocode";
+import { PromocodeNotFoundWithIdError, PromocodeNotFoundWithNameError } from "../../errors/promocode";
+import { IPromocodeGetMapper, IPromocodeCreateMapper, IPromocodeUpdateMapper } from "../../mappers/interfaces/promocode";
+import { PromocodeModel } from "../../models/promocode";
+import IPromocodeRepository from "../../repositories/interfaces/IPromocodeRepository";
+import IPromocodeService from "../interfaces/IPromocodeService";
+
+export default class PromocodeService implements IPromocodeService {
+
+    constructor(
+        protected promocodeGetMapper: IPromocodeGetMapper,
+        protected promocodeCreateMapper: IPromocodeCreateMapper,
+        protected promocodeUpdateMapper: IPromocodeUpdateMapper,
+        protected promocodeRepository: IPromocodeRepository
+    ) {}
+
+
+    public async getOne(id: number): Promise<PromocodeGetOutputDTO> {
+        const promocodeInstance = await this.promocodeRepository.getOne(id)
+
+        if (!promocodeInstance) {
+            throw new PromocodeNotFoundWithIdError(id)
+        }
+
+        return this.promocodeGetMapper.toDto(promocodeInstance, {})
+    }
+
+    public async getOneByName(name: string): Promise<PromocodeGetOutputDTO> {
+        const promocodeInstance = await this.promocodeRepository.getOneByName(name)
+
+        if (!promocodeInstance) {
+            throw new PromocodeNotFoundWithNameError(name)
+        }
+
+        return this.promocodeGetMapper.toDto(promocodeInstance, {})
+    }
+
+    public async getMany(): Promise<PromocodeGetOutputDTO[]> {
+        const promocodeInstances = await this.promocodeRepository.getMany()
+        return this.promocodeGetMapper.toDtos(promocodeInstances, [])
+    }
+    
+    public async create(data: PromocodeCreateInputDTO): Promise<PromocodeCreateOutputDTO> {
+        const promocodeCreateInput = await this.promocodeCreateMapper.toDbModel(data, {})
+        const promocodeCreatedInstance = await this.promocodeRepository.create(promocodeCreateInput)
+        return this.promocodeCreateMapper.toDto(promocodeCreatedInstance, {})
+    }
+
+    public async update(id: number, data: PromocodeUpdateInputDTO): Promise<PromocodeUpdateOutputDTO> {
+        const promocodeInstance = await this.promocodeRepository.getOne(id)
+
+        if (!promocodeInstance) {
+            throw new PromocodeNotFoundWithIdError(id)
+        }
+
+        const promocodeUpdateInput = await this.promocodeUpdateMapper.toDbModel(data, {})
+        
+        // if (promocodeUpdateInput.validFrom && promocodeUpdateInput.validFrom >= promocodeInstance.validUntil) {
+
+        // }
+
+        // if (promocodeUpdateInput.validUntil && promocodeInstance.validFrom >= promocodeUpdateInput.validUntil) {
+
+        // }
+
+        if (promocodeUpdateInput.maxUsageCount && promocodeUpdateInput.maxUsageCount > promocodeInstance.currentUsageCount) {
+
+        }
+
+        const promocodeUpdatedInstance = await this.promocodeRepository.update(id, promocodeUpdateInput) as PromocodeModel
+        return this.promocodeUpdateMapper.toDto(promocodeUpdatedInstance, {})
+    }
+
+    public async deactivate(id: number): Promise<undefined> {
+        const promocodeInstance = await this.promocodeRepository.getOne(id)
+
+        if (!promocodeInstance) {
+            throw new PromocodeNotFoundWithIdError(id)
+        }
+
+        await this.promocodeRepository.update(id, {
+            isActive: false
+        })
+    }
+
+    public async activate(id: number): Promise<undefined> {
+        const promocodeInstance = await this.promocodeRepository.getOne(id)
+
+        if (!promocodeInstance) {
+            throw new PromocodeNotFoundWithIdError(id)
+        }
+
+        await this.promocodeRepository.update(id, {
+            isActive: true
+        })
+    }
+
+}
---
+++ order/src/modules/promotions/services/instances/PromotionService.ts
@@ -0,0 +1,36 @@
+import { PromotionGetOutputDTO, PromotionCreateInputDTO, PromotionCreateOutputDTO } from "../../dto/promotion";
+import { PromotionNotFoundWithIdError } from "../../errors/promotion";
+import { IPromotionGetMapper, IPromotionCreateMapper } from "../../mappers/interfaces/promotion";
+import IPromotionRepository from "../../repositories/interfaces/IPromotionRepository";
+import IPromotionService from "../interfaces/IPromotionService";
+
+export default class PromotionService implements IPromotionService {
+
+    constructor(
+        protected promotionGetMapper: IPromotionGetMapper,
+        protected promotionCreateMapper: IPromotionCreateMapper,
+        protected promotionRepository: IPromotionRepository
+    ) {}
+
+    public async getOne(id: number): Promise<PromotionGetOutputDTO> {
+        const promotionInstance = await this.promotionRepository.getOne(id)
+
+        if (!promotionInstance) {
+            throw new PromotionNotFoundWithIdError(id)
+        }
+
+        return this.promotionGetMapper.toDto(promotionInstance, {})
+    }
+
+    public async getMany(): Promise<PromotionGetOutputDTO[]> {
+        const promotionInstances = await this.promotionRepository.getMany()
+        return this.promotionGetMapper.toDtos(promotionInstances, [])
+    }
+
+    public async create(data: PromotionCreateInputDTO): Promise<PromotionCreateOutputDTO> {
+        const promotionCreateInput = await this.promotionCreateMapper.toDbModel(data, {})
+        const promotionCreatedInstance = await this.promotionRepository.create(promotionCreateInput)
+        return this.promotionCreateMapper.toDto(promotionCreatedInstance, {})
+    }
+
+}
