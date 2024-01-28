d62250bc37d31cc14384d690909d48dc34209ef2
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-28 03:23:48 +0300

    added update dto

--- order/src/modules/promotions/dto/promocode.ts
+++ order/src/modules/promotions/dto/promocode.ts
@@ -1,19 +1,25 @@
 interface PromocodeBaseDTO {
-    nameIdentifier: string
     discountPercentage: number
     validFrom: string
     validUntil: string
     maxUsageCount: number
-    currentUsageCount: number
 }
 
 interface PromocodeBaseOutputDTO extends PromocodeBaseDTO {
     id: number
+    nameIdentifier: string
+    currentUsageCount: number
     isActive: boolean
 }
 
 export interface PromocodeGetOutputDTO extends PromocodeBaseOutputDTO {}
 
-export interface PromocodeCreateInputDTO extends PromocodeBaseDTO {}
+export interface PromocodeCreateInputDTO extends PromocodeBaseDTO {
+    nameIdentifier: string
+}
 
 export interface PromocodeCreateOutputDTO extends PromocodeBaseOutputDTO {}
+
+export interface PromocodeUpdateInputDTO extends PromocodeBaseDTO {}
+
+export interface PromocodeUpdateOutputDTO extends PromocodeBaseOutputDTO {}
