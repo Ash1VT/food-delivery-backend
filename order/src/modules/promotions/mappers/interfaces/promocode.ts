6749385d0d0f7450050a781206ff5eae9ecc218e
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-01-28 03:24:47 +0300

    added update interface mapper

--- order/src/modules/promotions/mappers/interfaces/promocode.ts
+++ order/src/modules/promotions/mappers/interfaces/promocode.ts
@@ -1,10 +1,13 @@
-import { PromocodeCreateOutputDTO, PromocodeCreateInputDTO, PromocodeGetOutputDTO } from "../../dto/promocode";
-import { PromocodeCreateInput, PromocodeModel } from "../../models/promocode";
-import { PromocodeCreateDtoModelAdditionalData, PromocodeCreateDbModelAdditionalData, PromocodeGetDtoModelAdditionalData } from "../additionalData";
+import { PromocodeCreateOutputDTO, PromocodeCreateInputDTO, PromocodeGetOutputDTO, PromocodeUpdateInputDTO, PromocodeUpdateOutputDTO } from "../../dto/promocode";
+import { PromocodeCreateInput, PromocodeModel, PromocodeUpdateInput } from "../../models/promocode";
+import { PromocodeCreateDtoModelAdditionalData, PromocodeCreateDbModelAdditionalData, PromocodeGetDtoModelAdditionalData, PromocodeUpdateDbModelAdditionalData, PromocodeUpdateDtoModelAdditionalData } from "../additionalData";
 import IDatabaseToDtoMapper from "@src/base/mappers/interfaces/IDatabaseToDtoMapper";
 import IDtoToDatabaseMapper from "@src/base/mappers/interfaces/IDtoToDatabaseMapper";
 
 export interface IPromocodeCreateMapper extends IDatabaseToDtoMapper<PromocodeModel, PromocodeCreateOutputDTO, PromocodeCreateDtoModelAdditionalData>,
                                               IDtoToDatabaseMapper<PromocodeCreateInputDTO, PromocodeCreateInput, PromocodeCreateDbModelAdditionalData> {}
+
+export interface IPromocodeUpdateMapper extends IDatabaseToDtoMapper<PromocodeModel, PromocodeUpdateOutputDTO, PromocodeUpdateDtoModelAdditionalData>,
+                                                IDtoToDatabaseMapper<PromocodeUpdateInputDTO, PromocodeUpdateInput, PromocodeUpdateDbModelAdditionalData> {}
 
 export interface IPromocodeGetMapper extends IDatabaseToDtoMapper<PromocodeModel, PromocodeGetOutputDTO, PromocodeGetDtoModelAdditionalData> {}
