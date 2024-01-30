b4f7a4c41f974f1b38a2f9f2feaddb7db96ff813
ash1vt <nikita.panasevitch@yandex.by>
2024-01-30 15:11:11 +0300

    refactoring of order repositories

--- order/src/modules/orders/repositories/interfaces/IOrderItemRepository.ts
+++ order/src/modules/orders/repositories/interfaces/IOrderItemRepository.ts
@@ -1,7 +1,4 @@
 import { OrderItemModel, OrderItemCreateInput, OrderItemUpdateInput } from "../../models/orderItem";
 import IBaseRepository from "@src/base/repositories/interfaces/IBaseRepository";
 
-export default interface IOrderItemRepository
-                         extends IBaseRepository<OrderItemModel, OrderItemCreateInput, OrderItemUpdateInput> {
-
-}
+export default interface IOrderItemRepository extends IBaseRepository<OrderItemModel, OrderItemCreateInput, OrderItemUpdateInput> {}
--- order/src/modules/orders/repositories/prisma/PrismaOrderRepository.ts
+++ order/src/modules/orders/repositories/prisma/PrismaOrderRepository.ts
@@ -3,6 +3,7 @@ import { OrderCreateInput, OrderModel, OrderUpdateInput } from "../../models/ord
 import PrismaBaseRepository from "@src/base/repositories/prisma/PrismaBaseRepository";
 import IOrderRepository from "../interfaces/IOrderRepository";
 import { OrderDelegate } from "./delegates";
+import { OrderStatus } from "../../models/orderStatus";
 
 
 export default class PrismaOrderRepository extends PrismaBaseRepository<OrderDelegate, OrderModel, OrderCreateInput, OrderUpdateInput>
@@ -12,13 +13,17 @@ export default class PrismaOrderRepository extends PrismaBaseRepository<OrderDel
         super(prisma.order)
     }
 
-    public async getOne(id: number): Promise<OrderModel | null> {
-        return await this.delegate.findFirst({
+    public async getMany(includeItems?: boolean, status?: OrderStatus): Promise<OrderModel[]> {
+        return await this.delegate.findMany({
             where: {
-                id
+                status
+            },
+            include: {
+                items: !!includeItems
             }
         })
     }
+
     public async create(data: OrderCreateInput): Promise<OrderModel> {
         return await this.delegate.create({
             data,
@@ -28,26 +33,38 @@ export default class PrismaOrderRepository extends PrismaBaseRepository<OrderDel
         })
     }
 
-    public async getCustomerOrders(customerId: number): Promise<OrderModel[]> {
+    public async getCustomerOrders(customerId: number, includeItems?: boolean, status?: OrderStatus): Promise<OrderModel[]> {
         return await this.delegate.findMany({
             where: {
-                customerId
+                customerId,
+                status
+            },
+            include: {
+                items: !!includeItems
             }
         })
     }
 
-    public async getRestaurantOrders(restaurantId: number): Promise<OrderModel[]> {
+    public async getRestaurantOrders(restaurantId: number, includeItems?: boolean, status?: OrderStatus): Promise<OrderModel[]> {
         return await this.delegate.findMany({
             where: {
-                restaurantId
+                restaurantId,
+                status
+            },
+            include: {
+                items: !!includeItems
             }
         })
     }
     
-    public async getCourierOrders(courierId: number): Promise<OrderModel[]> {
+    public async getCourierOrders(courierId: number, includeItems?: boolean, status?: OrderStatus): Promise<OrderModel[]> {
         return await this.delegate.findMany({
             where: {
-                courierId
+                courierId,
+                status
+            },
+            include: {
+                items: !!includeItems
             }
         })
     }
