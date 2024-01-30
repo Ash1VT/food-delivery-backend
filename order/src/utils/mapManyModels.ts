e3ef7375d1483cdd83dcc0bb53be68146b44b2d8
ash1vt <nikita.panasevitch@yandex.by>
2024-01-30 15:12:22 +0300

    added different functions for mapping many instances

--- order/src/utils/mapManyModels.ts
+++ order/src/utils/mapManyModels.ts
@@ -1,4 +1,8 @@
-export default function mapManyModels<SourceModel, ResultModel, AdditionalData>(sourceModels: SourceModel[], mapSingleModel: {(model: SourceModel, additionalData: AdditionalData): ResultModel}, additionalData: AdditionalData[]) {
+export function mapManyModels<SourceModel, ResultModel>(sourceModels: SourceModel[], mapSingleModel: {(model: SourceModel): ResultModel}): ResultModel[] {
+    return sourceModels.map((sourceModel) => mapSingleModel(sourceModel))
+}
+
+export function mapManyModelsWithAdditionalData<SourceModel, ResultModel, AdditionalData>(sourceModels: SourceModel[], mapSingleModel: {(model: SourceModel, additionalData: AdditionalData): ResultModel}, additionalData: AdditionalData[]): ResultModel[] {
     return sourceModels.map((sourceModel, index) => {
         const data = additionalData[index]
         return mapSingleModel(sourceModel, data)
