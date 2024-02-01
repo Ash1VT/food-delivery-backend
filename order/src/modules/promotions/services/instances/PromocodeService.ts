7e5ce2795f0cad55995f162e850d321d8ffcac38
ash1vt <nikita.panasevitch@yandex.by>
2024-02-01 18:18:07 +0300

    added checking user permissions

--- order/src/modules/promotions/services/instances/PromocodeService.ts
+++ order/src/modules/promotions/services/instances/PromocodeService.ts
@@ -1,3 +1,5 @@
+import { PermissionDeniedError } from './../../../users/errors/permissions';
+import { RestaurantManagerModel } from './../../../users/models/restaurantManager';
 import { PromocodeGetOutputDTO, PromocodeCreateInputDTO, PromocodeCreateOutputDTO, PromocodeUpdateInputDTO, PromocodeUpdateOutputDTO } from "../../dto/promocode";
 import { PromocodeNotFoundWithIdError, PromocodeNotFoundWithNameError, PromocodeUsageError } from "../../errors/promocode";
 import { IPromocodeGetMapper, IPromocodeCreateMapper, IPromocodeUpdateMapper } from "../../mappers/interfaces/promocode";
@@ -7,6 +9,7 @@ import IPromocodeRepository from "../../repositories/interfaces/IPromocodeReposi
 import IPromocodeService from "../interfaces/IPromocodeService";
 import { RestaurantNotFoundWithIdError } from "@src/modules/restaurants/errors/restaurant";
 import { mapManyModels } from "@src/utils/mapManyModels";
+import { RestaurantManagerOwnershipError } from '@src/modules/users/errors/restaurantManager';
 
 export default class PromocodeService implements IPromocodeService {
 
@@ -15,93 +18,154 @@ export default class PromocodeService implements IPromocodeService {
         protected promocodeCreateMapper: IPromocodeCreateMapper,
         protected promocodeUpdateMapper: IPromocodeUpdateMapper,
         protected promocodeRepository: IPromocodeRepository,
-        protected restaurantRepository: IRestaurantRepository
+        protected restaurantRepository: IRestaurantRepository,
+        protected restaurantManager?: RestaurantManagerModel
     ) {}
 
 
-    public async getOne(id: number): Promise<PromocodeGetOutputDTO> {
-        const promocodeInstance = await this.promocodeRepository.getOne(id)
+    // public async getOne(promocodeId: number): Promise<PromocodeGetOutputDTO> {
 
-        if (!promocodeInstance) {
-            throw new PromocodeNotFoundWithIdError(id)
+    //     if (!this.restaurantManager) {
+    //         throw new PermissionDeniedError()
+    //     }
+
+    //     const promocodeInstance = await this.promocodeRepository.getOne(promocodeId)
+
+    //     if (!promocodeInstance) {
+    //         throw new PromocodeNotFoundWithIdError(promocodeId)
+    //     }
+
+    //     if (promocodeInstance.restaurantId !== this.restaurantManager.restaurantId) {
+    //         throw new RestaurantManagerOwnershipError(this.restaurantManager.id, promocodeInstance.restaurantId)
+    //     }
+
+    //     return this.promocodeGetMapper.toDto(promocodeInstance)
+    // }
+
+    // public async getOneByName(promocodeName: string): Promise<PromocodeGetOutputDTO> {
+
+    //     if (!this.restaurantManager) {
+    //         throw new PermissionDeniedError()
+    //     }
+
+    //     const promocodeInstance = await this.promocodeRepository.getOneByName(promocodeName)
+
+    //     if (!promocodeInstance) {
+    //         throw new PromocodeNotFoundWithNameError(promocodeName)
+    //     }
+
+    //     if (promocodeInstance.restaurantId !== this.restaurantManager.restaurantId) {
+    //         throw new RestaurantManagerOwnershipError(this.restaurantManager.id, promocodeInstance.restaurantId)
+    //     }
+
+    //     return this.promocodeGetMapper.toDto(promocodeInstance)
+    // }
+
+    // public async getMany(): Promise<PromocodeGetOutputDTO[]> {
+    //     const promocodeInstances = await this.promocodeRepository.getMany()
+    //     return mapManyModels(promocodeInstances, this.promocodeGetMapper.toDto)
+    // }
+
+    public async getRestaurantPromocodes(restaurantId: number): Promise<PromocodeGetOutputDTO[]> {
+
+        if (!this.restaurantManager) {
+            throw new PermissionDeniedError()
+        }
+        
+        if (Number(this.restaurantManager.restaurantId) !== restaurantId) {
+            throw new RestaurantManagerOwnershipError(this.restaurantManager.id, restaurantId)
         }
 
-        return this.promocodeGetMapper.toDto(promocodeInstance)
-    }
-
-    public async getOneByName(name: string): Promise<PromocodeGetOutputDTO> {
-        const promocodeInstance = await this.promocodeRepository.getOneByName(name)
-
-        if (!promocodeInstance) {
-            throw new PromocodeNotFoundWithNameError(name)
-        }
-
-        return this.promocodeGetMapper.toDto(promocodeInstance)
-    }
-
-    public async getMany(): Promise<PromocodeGetOutputDTO[]> {
-        const promocodeInstances = await this.promocodeRepository.getMany()
+        const promocodeInstances = await this.promocodeRepository.getRestaurantPromocodes(restaurantId)
         return mapManyModels(promocodeInstances, this.promocodeGetMapper.toDto)
     }
-    
-    public async create(data: PromocodeCreateInputDTO): Promise<PromocodeCreateOutputDTO> {
-        const promocodeCreateInput = this.promocodeCreateMapper.toDbModel(data)
 
-        const restaurantInstance = await this.restaurantRepository.getOne(data.restaurantId)
+    public async create(promocodeData: PromocodeCreateInputDTO): Promise<PromocodeCreateOutputDTO> {
+        
+        if (!this.restaurantManager) {
+            throw new PermissionDeniedError()
+        }
+
+        const promocodeCreateInput = this.promocodeCreateMapper.toDbModel(promocodeData)
+
+        const restaurantInstance = await this.restaurantRepository.getOne(promocodeData.restaurantId)
 
         if (!restaurantInstance) {
-            throw new RestaurantNotFoundWithIdError(data.restaurantId)
+            throw new RestaurantNotFoundWithIdError(promocodeData.restaurantId)
+        }
+
+        if (this.restaurantManager.restaurantId !== promocodeCreateInput.restaurantId) {
+            throw new RestaurantManagerOwnershipError(this.restaurantManager.id, promocodeCreateInput.restaurantId)
         }
 
         const promocodeCreatedInstance = await this.promocodeRepository.create(promocodeCreateInput)
         return this.promocodeCreateMapper.toDto(promocodeCreatedInstance)
     }
 
-    public async update(id: number, data: PromocodeUpdateInputDTO): Promise<PromocodeUpdateOutputDTO> {
-        const promocodeInstance = await this.promocodeRepository.getOne(id)
+    public async update(promocodeId: number, promocodeData: PromocodeUpdateInputDTO): Promise<PromocodeUpdateOutputDTO> {
+
+        if (!this.restaurantManager) {
+            throw new PermissionDeniedError()
+        }
+
+        const promocodeInstance = await this.promocodeRepository.getOne(promocodeId)
 
         if (!promocodeInstance) {
-            throw new PromocodeNotFoundWithIdError(id)
+            throw new PromocodeNotFoundWithIdError(promocodeId)
         }
 
-        const promocodeUpdateInput = this.promocodeUpdateMapper.toDbModel(data)
-        
-        // if (promocodeUpdateInput.validFrom && promocodeUpdateInput.validFrom >= promocodeInstance.validUntil) {
+        if (this.restaurantManager.restaurantId !== promocodeInstance.restaurantId) {
+            throw new RestaurantManagerOwnershipError(this.restaurantManager.id, promocodeInstance.restaurantId)
+        }
 
-        // }
-
-        // if (promocodeUpdateInput.validUntil && promocodeInstance.validFrom >= promocodeUpdateInput.validUntil) {
-
-        // }
+        const promocodeUpdateInput = this.promocodeUpdateMapper.toDbModel(promocodeData)
 
         if (promocodeUpdateInput.maxUsageCount && promocodeUpdateInput.maxUsageCount > promocodeInstance.currentUsageCount) {
-            throw new PromocodeUsageError(id)
+            throw new PromocodeUsageError(promocodeId)
         }
 
-        const promocodeUpdatedInstance = await this.promocodeRepository.update(id, promocodeUpdateInput) as PromocodeModel
+        const promocodeUpdatedInstance = await this.promocodeRepository.update(promocodeId, promocodeUpdateInput) as PromocodeModel
         return this.promocodeUpdateMapper.toDto(promocodeUpdatedInstance)
     }
 
-    public async deactivate(id: number): Promise<undefined> {
-        const promocodeInstance = await this.promocodeRepository.getOne(id)
+    public async deactivate(promocodeId: number): Promise<void> {
 
-        if (!promocodeInstance) {
-            throw new PromocodeNotFoundWithIdError(id)
+        if (!this.restaurantManager) {
+            throw new PermissionDeniedError()
         }
 
-        await this.promocodeRepository.update(id, {
+        const promocodeInstance = await this.promocodeRepository.getOne(promocodeId)
+
+        if (!promocodeInstance) {
+            throw new PromocodeNotFoundWithIdError(promocodeId)
+        }
+
+        if (this.restaurantManager.restaurantId !== promocodeInstance.restaurantId) {
+            throw new RestaurantManagerOwnershipError(this.restaurantManager.id, promocodeInstance.restaurantId)
+        }
+
+        await this.promocodeRepository.update(promocodeId, {
             isActive: false
         })
     }
 
-    public async activate(id: number): Promise<undefined> {
-        const promocodeInstance = await this.promocodeRepository.getOne(id)
+    public async activate(promocodeId: number): Promise<void> {
 
-        if (!promocodeInstance) {
-            throw new PromocodeNotFoundWithIdError(id)
+        if (!this.restaurantManager) {
+            throw new PermissionDeniedError()
         }
 
-        await this.promocodeRepository.update(id, {
+        const promocodeInstance = await this.promocodeRepository.getOne(promocodeId)
+
+        if (!promocodeInstance) {
+            throw new PromocodeNotFoundWithIdError(promocodeId)
+        }
+
+        if (this.restaurantManager.restaurantId !== promocodeInstance.restaurantId) {
+            throw new RestaurantManagerOwnershipError(this.restaurantManager.id, promocodeInstance.restaurantId)
+        }
+
+        await this.promocodeRepository.update(promocodeId, {
             isActive: true
         })
     }
