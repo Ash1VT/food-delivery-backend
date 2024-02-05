3517f341faa5baa6af4ffbdd02053570fcf07dc1
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-02-05 12:50:28 +0300

    started with tests for services

---
+++ order/tests/services.test.ts
@@ -0,0 +1,63 @@
+describe("Tests for Services", () => {
+
+    // COURIER
+
+    describe('Tests for Courier Service', () => {
+
+    })
+
+    // CUSTOMER
+
+    describe('Tests for Customer Service', () => {
+
+    })
+
+    // MODERATOR
+
+    describe('Tests for Moderator Service', () => {
+
+    })
+
+    // RESTAURANT MANAGER
+
+    describe('Tests for Restaurant Manager Service', () => {
+
+    })
+
+    // RESTAURANT
+
+    describe('Tests for Restaurant Service', () => {
+
+    })
+
+    // MENU ITEM
+
+    describe('Tests for Menu Item Service', () => {
+
+    })
+
+    // PROMOTION
+
+    describe('Tests for Promotion Service', () => {
+
+    })
+
+    // PROMOCODE
+
+    describe('Tests for Promocode Service', () => {
+
+    })
+
+    // ORDER ITEM
+
+    describe('Tests for Order Item Service', () => {
+
+    })
+
+    // ORDER
+
+    describe('Tests for Order Service', () => {
+
+    })
+
+})
