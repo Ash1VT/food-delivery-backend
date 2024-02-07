5a51bdd6f48b1b4af5cea98c45c149a6e7d7f499
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-02-08 02:18:44 +0300

    fixed throwing type error when passing undefined

--- order/src/core/validators/idValidator.ts
+++ order/src/core/validators/idValidator.ts
@@ -1,5 +1,8 @@
 import { z } from "zod";
 
-export const idValidator = z.coerce.bigint().nonnegative({
+export const idValidator = z.any().transform( value => {
+    try { return BigInt( value ) }
+    catch ( error ) { return value }
+} ).pipe( z.bigint().nonnegative({
     message: "Id must be nonnegative"
-})
+}))
