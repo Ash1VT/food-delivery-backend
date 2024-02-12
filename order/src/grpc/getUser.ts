4f6ab9c3204d417d2cd41c3ec6ed7301ac0d18f5
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-02-12 03:35:25 +0300

    added type for grpc user

--- order/src/grpc/getUser.ts
+++ order/src/grpc/getUser.ts
@@ -1,18 +1,21 @@
 import { client } from "./client";
-import { GetUserRoleRequest } from "./generated/roles_pb";
-import { GetUserRoleResponse } from './generated/roles/GetUserRoleResponse';
+import { GetUserRoleRequest } from "./generated/roles/roles_pb";
+import { GrpcUser } from "./user.type";
 
-export async function getUser(accessToken: string): Promise<GetUserRoleResponse> {
+export async function getUser(accessToken: string): Promise<GrpcUser> {
     const getUserRoleRequest = new GetUserRoleRequest()
     getUserRoleRequest.setAccessToken(accessToken)
 
-    return await new Promise<GetUserRoleResponse>((resolve, reject) => {
+    return await new Promise<GrpcUser>((resolve, reject) => {
         client.getUserRole(getUserRoleRequest, (err, res) => {
             if (err) {
                 reject(err)
             }
             if (res) {
-                resolve(res.toObject())
+                resolve({
+                    userId: res.getUserId(),
+                    role: res.getRole()
+                })
             }
         })
     })
