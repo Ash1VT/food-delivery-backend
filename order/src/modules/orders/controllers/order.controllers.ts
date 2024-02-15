65bd20b43d1d29f1fa582100298a5f6999609c8e
ash1vt <nikita.panasevitch@yandex.by>
2024-02-16 02:56:21 +0300

    finished with order module controllers

--- order/src/modules/orders/controllers/order.controllers.ts
+++ order/src/modules/orders/controllers/order.controllers.ts
@@ -1,138 +1,127 @@
-import { orderCreateValidator } from '@src/modules/orders/validators/order.validators';
+import { orderCreateValidator, orderStatusValidator } from '@src/modules/orders/validators/order.validators';
 import { getPrismaClient } from "./../../../core/setup/prisma";
 import { Request, Response } from "express"
 import PrismaOrderServiceFactory from '../services/factories/implementations/prisma/PrismaOrderServiceFactory';
-import { authenticate } from '@src/modules/authentication/utils/authenticate';
-import { authenticateService } from '@src/modules/authentication/utils/authenticateService';
-import PrismaUserRepositoryFactory from '@src/modules/users/repositories/factories/implementations/prisma/PrismaUserRepositoryFactory';
 import { idValidator } from '@src/core/validators/idValidator';
+import { authenticateWithPrisma } from '@src/modules/authentication/utils/prisma/auhenticateWithPrisma';
 
-export const createOrder = async (req: Request, res: Response) => {
-    const orderCreateInputDto = orderCreateValidator.parse(req.body)
+export const makeOrder = async (req: Request, res: Response) => {
     const prismaClient = getPrismaClient()
-    
+
+    const orderCreateInputDto = orderCreateValidator.parse(req.body)
+
     const orderServiceFactory = new PrismaOrderServiceFactory(prismaClient)
-    const userRepositoryFactory = new PrismaUserRepositoryFactory(prismaClient)
-
-    const accessToken = req.cookies.access_token
-
-    const user = await authenticate(accessToken)
-
     const orderService = orderServiceFactory.createOrderService()
+    
+    await authenticateWithPrisma(req, prismaClient, orderService)
 
-    await authenticateService(orderService, userRepositoryFactory, user)
     const orderCreateOutputDto = await orderService.makeOrder(orderCreateInputDto)
     
-    res.json(orderCreateOutputDto)
+    res.status(201).json(orderCreateOutputDto)
+}
+
+export const getAllOrders = async (req: Request, res: Response) => {
+    const prismaClient = getPrismaClient()
+    
+    const orderStatus = orderStatusValidator.parse(req.query.status)
+    
+    const orderServiceFactory = new PrismaOrderServiceFactory(prismaClient)
+    const orderService = orderServiceFactory.createOrderService()
+
+    await authenticateWithPrisma(req, prismaClient, orderService)
+
+    const orderGetOutputDtos = await orderService.getMany(orderStatus)
+    
+    res.status(200).json(orderGetOutputDtos)
 }
 
 export const getCurrentCustomerOrders = async (req: Request, res: Response) => {
     const prismaClient = getPrismaClient()
     
+    const orderStatus = orderStatusValidator.parse(req.query.status)
+    
     const orderServiceFactory = new PrismaOrderServiceFactory(prismaClient)
-    const userRepositoryFactory = new PrismaUserRepositoryFactory(prismaClient)
-
-    const accessToken = req.cookies.access_token
-
-    const user = await authenticate(accessToken)
-
     const orderService = orderServiceFactory.createOrderService()
 
-    await authenticateService(orderService, userRepositoryFactory, user)
-    const orderCreateOutputDtos = await orderService.getCurrentCustomerOrders()
+    await authenticateWithPrisma(req, prismaClient, orderService)
+
+    const orderGetOutputDtos = await orderService.getCurrentCustomerOrders(orderStatus)
     
-    res.json(orderCreateOutputDtos)
+    res.status(200).json(orderGetOutputDtos)
 }
 
 export const getCurrentCourierOrders = async (req: Request, res: Response) => {
     const prismaClient = getPrismaClient()
+   
+    const orderStatus = orderStatusValidator.parse(req.query.status)
     
     const orderServiceFactory = new PrismaOrderServiceFactory(prismaClient)
-    const userRepositoryFactory = new PrismaUserRepositoryFactory(prismaClient)
-
-    const accessToken = req.cookies.access_token
-
-    const user = await authenticate(accessToken)
-
     const orderService = orderServiceFactory.createOrderService()
 
-    await authenticateService(orderService, userRepositoryFactory, user)
-    const orderCreateOutputDtos = await orderService.getCurrentCourierOrders()
+    await authenticateWithPrisma(req, prismaClient, orderService)
+
+    const orderGetOutputDtos = await orderService.getCurrentCourierOrders(orderStatus)
     
-    res.json(orderCreateOutputDtos)
+    res.status(200).json(orderGetOutputDtos)
 }
 
 export const getAvailableForDeliveryOrders = async (req: Request, res: Response) => {
     const prismaClient = getPrismaClient()
     
     const orderServiceFactory = new PrismaOrderServiceFactory(prismaClient)
-    const userRepositoryFactory = new PrismaUserRepositoryFactory(prismaClient)
-
-    const accessToken = req.cookies.access_token
-
-    const user = await authenticate(accessToken)
-
     const orderService = orderServiceFactory.createOrderService()
 
-    await authenticateService(orderService, userRepositoryFactory, user)
-    const orderCreateOutputDtos = await orderService.getReadyOrders()
+    await authenticateWithPrisma(req, prismaClient, orderService)
+
+    const orderGetOutputDtos = await orderService.getReadyOrders()
     
-    res.json(orderCreateOutputDtos)
+    res.status(200).json(orderGetOutputDtos)
 }
 
 export const getRestaurantOrders = async (req: Request, res: Response) => {
     const prismaClient = getPrismaClient()
     
+    const orderStatus = orderStatusValidator.parse(req.query.status)
+    const restaurantId = idValidator.parse(req.params.id)
+
     const orderServiceFactory = new PrismaOrderServiceFactory(prismaClient)
-    const userRepositoryFactory = new PrismaUserRepositoryFactory(prismaClient)
-
-    const accessToken = req.cookies.access_token
-
-    const user = await authenticate(accessToken)
-
     const orderService = orderServiceFactory.createOrderService()
 
-    await authenticateService(orderService, userRepositoryFactory, user)
-    const orderCreateOutputDtos = await orderService.getReadyOrders()
+    await authenticateWithPrisma(req, prismaClient, orderService)
+
+    const orderGetOutputDtos = await orderService.getRestaurantOrders(restaurantId, orderStatus)
     
-    res.json(orderCreateOutputDtos)
+    res.status(200).json(orderGetOutputDtos)
 }
 
 
 export const takeOrder = async (req: Request, res: Response) => {
     const prismaClient = getPrismaClient()
-    
-    const orderServiceFactory = new PrismaOrderServiceFactory(prismaClient)
-    const userRepositoryFactory = new PrismaUserRepositoryFactory(prismaClient)
 
-    const accessToken = req.cookies.access_token
     const orderId = idValidator.parse(req.params.id)
 
-    const user = await authenticate(accessToken)
-
+    const orderServiceFactory = new PrismaOrderServiceFactory(prismaClient)
     const orderService = orderServiceFactory.createOrderService()
 
-    await authenticateService(orderService, userRepositoryFactory, user)
-    const orderCreateOutputDto = await orderService.takeOrder(orderId)
+    await authenticateWithPrisma(req, prismaClient, orderService)
+
+    await orderService.takeOrder(orderId)
     
-    res.json(orderCreateOutputDto)
+    res.status(200).json({})
 }
 
 export const finishOrderDelivery = async (req: Request, res: Response) => {
     const prismaClient = getPrismaClient()
+
+    const orderId = idValidator.parse(req.params.id)
     
     const orderServiceFactory = new PrismaOrderServiceFactory(prismaClient)
-    const userRepositoryFactory = new PrismaUserRepositoryFactory(prismaClient)
-
-    const accessToken = req.cookies.access_token
-    const orderId = idValidator.parse(req.params.id)
-
-    const user = await authenticate(accessToken)
-
     const orderService = orderServiceFactory.createOrderService()
 
-    await authenticateService(orderService, userRepositoryFactory, user)
-    const orderCreateOutputDto = await orderService.finishOrderDelivery(orderId)
+    await authenticateWithPrisma(req, prismaClient, orderService)
+
+    await orderService.finishOrderDelivery(orderId)
     
-    res.json(orderCreateOutputDto)
+    res.status(200).json({})
 }
+
