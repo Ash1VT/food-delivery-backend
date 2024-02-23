9bd83da11a3e22a330035e191de39e4ceb8ed9fd
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-02-24 01:24:05 +0300

    updated producer schemas and events

--- restaurant/src/producer/schemas.py
+++ restaurant/src/producer/schemas.py
@@ -1,35 +1,27 @@
 from pydantic import BaseModel, Field
 
 __all__ = [
-    "RestaurantActivatedSchema",
-    "RestaurantDeactivatedSchema",
-    "RestaurantApplicationConfirmedSchema",
+    "RestaurantCreatedSchema",
+    "RestaurantUpdatedSchema"
 ]
 
 
-class RestaurantActivatedSchema(BaseModel):
+class RestaurantCreatedSchema(BaseModel):
     """
-    Schema class for output representation of data of restaurant when it is activated
-    to data that will be published to Kafka.
-    """
-
-    id: int = Field(ge=0)
-
-
-class RestaurantDeactivatedSchema(BaseModel):
-    """
-    Schema class for output representation of data of restaurant when it is deactivated
-    to data that will be published to Kafka.
-    """
-
-    id: int = Field(ge=0)
-
-
-class RestaurantApplicationConfirmedSchema(BaseModel):
-    """
-    Schema class for output representation of data of restaurant when restaurant application is confirmed
+    Schema class for output representation of data of restaurant when restaurant is created
     to data that will be published to Kafka.
     """
 
     id: int = Field(ge=0)
     restaurant_manager_id: int = Field(ge=0)
+    is_active: bool
+
+
+class RestaurantUpdatedSchema(BaseModel):
+    """
+    Schema class for output representation of data of restaurant when it is updated
+    to data that will be published to Kafka.
+    """
+
+    id: int = Field(ge=0)
+    is_active: bool
