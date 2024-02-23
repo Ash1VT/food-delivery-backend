1f25c8af90661ef0cb0a38eae4d462c9b25bb4d0
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-02-24 00:54:52 +0300

    added new events for creating customer and courier and corresponding serializers

--- user-management/src/producer/serializers.py
+++ user-management/src/producer/serializers.py
@@ -1,6 +1,22 @@
 from rest_framework import serializers
 
 
+class CustomerCreatedSerializer(serializers.Serializer):
+    """
+    Serializes Customer to data that will be published to Kafka.
+    """
+
+    id = serializers.IntegerField(min_value=0)
+
+
+class CourierCreatedSerializer(serializers.Serializer):
+    """
+    Serializes Courier to data that will be published to Kafka.
+    """
+
+    id = serializers.IntegerField(min_value=0)
+
+
 class RestaurantManagerCreatedSerializer(serializers.Serializer):
     """
     Serializes RestaurantManager to data that will be published to Kafka.
