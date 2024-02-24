c310ee93f100de43ad10170aed96805d8045fb1b
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-02-24 03:35:39 +0300

    added error when restaurant is not active

--- order/src/modules/restaurants/errors/restaurant.errors.ts
+++ order/src/modules/restaurants/errors/restaurant.errors.ts
@@ -1,3 +1,4 @@
+import AppError from "@src/core/errors/AppError";
 import DatabaseInstanceAlreadyExistsError from "@src/core/errors/DatabaseInstanceAlreadyExistsError";
 import DatabaseInstanceNotFoundError from "@src/core/errors/DatabaseInstanceNotFoundError";
 
@@ -13,6 +14,19 @@ export class RestaurantAlreadyExistsWithIdError extends DatabaseInstanceAlreadyE
 
     constructor(restaurantId: bigint) {
         super("id", restaurantId.toString(), "Restaurant")
+    }
+
+}
+
+
+export class RestaurantNotActiveError extends AppError {
+
+    constructor(restaurantId: bigint) {
+        super(`Restaurant with id=${restaurantId} is not active`)
+    }
+
+    public get statusCode(): number {
+        return 403
     }
 
 }
