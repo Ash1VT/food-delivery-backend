83cc9a5af8505360819e162483c07acd61785867
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-02-24 03:48:53 +0300

    added building settings for kafka

--- order/src/config/SettingsBuilder.ts
+++ order/src/config/SettingsBuilder.ts
@@ -1,5 +1,11 @@
+import { CustomerCreatedPrismaEvent } from "@src/modules/users/consumer/events/prisma/customer.events";
 import EnvManager from "./EnvManager";
 import { Settings } from "./Settings";
+import { CourierCreatedPrismaEvent } from "@src/modules/users/consumer/events/prisma/courier.events";
+import { ModeratorCreatedPrismaEvent } from "@src/modules/users/consumer/events/prisma/moderator.events";
+import { RestaurantManagerCreatedPrismaEvent } from "@src/modules/users/consumer/events/prisma/restaurantManager.events";
+import { MenuItemCreatedPrismaEvent, MenuItemDeletedPrismaEvent, MenuItemUpdatedPrismaEvent } from "@src/modules/menu/consumer/events/prisma/menuItem.events";
+import { RestaurantCreatedPrismaEvent, RestaurantUpdatedPrismaEvent } from "@src/modules/restaurants/consumer/events/prisma/restaurant.events";
 
 export abstract class BaseSettingsBuilder {
     public abstract build(): Settings;
@@ -18,7 +24,26 @@ export class SettingsBuilder extends BaseSettingsBuilder {
             "appHost": EnvManager.getVariable("APP_HOST"),
             "appPort": Number.parseInt(EnvManager.getVariable("APP_PORT")),
             "rolesGrpcServerHost": EnvManager.getVariable("ROLES_GRPC_SERVER_HOST"),
-            "rolesGrpcServerPort": Number(EnvManager.getVariable("ROLES_GRPC_SERVER_PORT"))
+            "rolesGrpcServerPort": Number(EnvManager.getVariable("ROLES_GRPC_SERVER_PORT")),
+            "kafkaBootstrapServerHost": EnvManager.getVariable("KAFKA_BOOTSTRAP_SERVER_HOST"),
+            "kafkaBootstrapServerPort": Number(EnvManager.getVariable("KAFKA_BOOTSTRAP_SERVER_PORT")),
+            "kafkaBrokerUser": EnvManager.getVariable("KAFKA_BROKER_USER"),
+            "kafkaBrokerPassword": EnvManager.getVariable("KAFKA_BROKER_PASSWORD"),
+            "kafkaGroupConsumersCount": 1,
+            "kafkaConsumerTopicsEvents": [
+                {
+                    topicName: "user_order",
+                    events: [CustomerCreatedPrismaEvent, CourierCreatedPrismaEvent, ModeratorCreatedPrismaEvent, RestaurantManagerCreatedPrismaEvent]
+                },
+                {
+                    topicName: "menu_order",
+                    events: [MenuItemCreatedPrismaEvent, MenuItemUpdatedPrismaEvent, MenuItemDeletedPrismaEvent]
+                },
+                {
+                    topicName: "restaurant_order",
+                    events: [RestaurantCreatedPrismaEvent, RestaurantUpdatedPrismaEvent]
+                }
+            ]
         })
     }
 }
