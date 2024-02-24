d1600dd965c43c84c601ceb5fcbb1cae67b7e28b
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-02-24 03:38:10 +0300

    changed factory

--- order/src/modules/restaurants/services/factories/implementations/prisma/PrismaRestaurantServiceFactory.ts
+++ order/src/modules/restaurants/services/factories/implementations/prisma/PrismaRestaurantServiceFactory.ts
@@ -6,19 +6,25 @@ import IRestaurantServiceFactory from "../../interfaces/IRestaurantServiceFactor
 import { PrismaClient } from "@prisma/client";
 import RestaurantMapperFactory from "@src/modules/restaurants/mappers/factories/implementations/RestaurantMapperFactory";
 import PrismaRestaurantRepositoryFactory from "@src/modules/restaurants/repositories/factories/implementations/prisma/PrismaRestaurantRepositoryFactory";
+import IUserRepositoryFactory from "@src/modules/users/repositories/factories/interfaces/IUserRepositoryFactory";
+import PrismaUserRepositoryFactory from "@src/modules/users/repositories/factories/implementations/prisma/PrismaUserRepositoryFactory";
 
 export default class PrismaRestaurantServiceFactory implements IRestaurantServiceFactory {
     protected restaurantMapperFactory: IRestaurantMapperFactory = new RestaurantMapperFactory()
     protected restaurantRepositoryFactory: IRestaurantRepositoryFactory
+    protected userRepositoryFactory: IUserRepositoryFactory
 
     constructor(prismaClient: PrismaClient) {
         this.restaurantRepositoryFactory = new PrismaRestaurantRepositoryFactory(prismaClient)
+        this.userRepositoryFactory = new PrismaUserRepositoryFactory(prismaClient)
     }
 
     public createRestaurantService(): IRestaurantService {
         return new RestaurantService(
             this.restaurantMapperFactory.createRestaurantCreateMapper(),
-            this.restaurantRepositoryFactory.createRestaurantRepository()
+            this.restaurantMapperFactory.createRestaurantUpdateMapper(),
+            this.restaurantRepositoryFactory.createRestaurantRepository(),
+            this.userRepositoryFactory.createRestaurantManagerRepository()
         );
     }
 }
