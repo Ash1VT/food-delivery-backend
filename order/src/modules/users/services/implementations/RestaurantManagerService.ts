95ecb932cfc2592e828c3b6e04c3e38f554f2756
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-02-24 03:39:41 +0300

    added update method

--- order/src/modules/users/services/implementations/RestaurantManagerService.ts
+++ order/src/modules/users/services/implementations/RestaurantManagerService.ts
@@ -1,13 +1,16 @@
 
 import BaseService from "@src/core/services/BaseService";
-import { RestaurantManagerCreateInputDto, RestaurantManagerCreateOutputDto } from "../../dto/restaurantManager.dto";
-import { IRestaurantManagerCreateMapper } from "../../mappers/interfaces/restaurantManager.mappers";
+import { RestaurantManagerCreateInputDto, RestaurantManagerCreateOutputDto, RestaurantManagerUpdateInputDto, RestaurantManagerUpdateOutputDto } from "../../dto/restaurantManager.dto";
+import { IRestaurantManagerCreateMapper, IRestaurantManagerUpdateMapper } from "../../mappers/interfaces/restaurantManager.mappers";
 import IRestaurantManagerRepository from "../../repositories/interfaces/IRestaurantManagerRepository";
 import IRestaurantManagerService from "../interfaces/IRestaurantManagerService";
+import { RestaurantManagerNotFoundWithIdError } from "../../errors/restaurantManager.errors";
+import { RestaurantManagerModel } from "../../models/restaurantManager.models";
 
 export default class RestaurantManagerService extends BaseService implements IRestaurantManagerService {
     constructor(
         protected restaurantManagerCreateMapper: IRestaurantManagerCreateMapper,
+        protected restaurantManagerUpdateMapper: IRestaurantManagerUpdateMapper,
         protected restaurantManagerRepository: IRestaurantManagerRepository
     ) {
         super()
@@ -17,6 +20,18 @@ export default class RestaurantManagerService extends BaseService implements IRe
         const restaurantManagerCreateInput = this.restaurantManagerCreateMapper.toDbModel(restaurantManagerData)
         const restaurantManagerCreatedInstance = await this.restaurantManagerRepository.create(restaurantManagerCreateInput)
         return this.restaurantManagerCreateMapper.toDto(restaurantManagerCreatedInstance)
+    }
+    
+    public async update(restaurantManagerId: bigint, restaurantManagerData: RestaurantManagerUpdateInputDto): Promise<RestaurantManagerUpdateOutputDto> {
+        const restaurantManagerInstance = await this.restaurantManagerRepository.getOne(restaurantManagerId)
+
+        if (!restaurantManagerInstance) {
+            throw new RestaurantManagerNotFoundWithIdError(restaurantManagerId)
+        }
+
+        const restaurantManagerUpdateInput = this.restaurantManagerUpdateMapper.toDbModel(restaurantManagerData)
+        const restaurantManagerUpdatedInstance = await this.restaurantManagerRepository.update(restaurantManagerId, restaurantManagerUpdateInput) as RestaurantManagerModel
+        return this.restaurantManagerUpdateMapper.toDto(restaurantManagerUpdatedInstance)
     }
 
 }
