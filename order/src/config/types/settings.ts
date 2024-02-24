068d7b96ed1f8ec31b92d97cdef33152a6db74a6
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-02-24 03:42:49 +0300

    added kafka properties in settings

--- order/src/config/types/settings.ts
+++ order/src/config/types/settings.ts
@@ -1,3 +1,16 @@
+import KafkaBaseEvent from "@src/kafka/consumer/events/KafkaBaseEvent"
+
+export interface KafkaEventConstructor {
+    new (data: object): KafkaBaseEvent
+    getEventName(): string
+}
+
+export type KafkaConsumerTopicEvent = {
+    topicName: string
+    events: KafkaEventConstructor[]
+}
+
+
 export type SettingsOptions = {
     pgHost: string
     pgPort: string
@@ -7,6 +20,15 @@ export type SettingsOptions = {
     databaseURL: string
     appHost: string
     appPort: number
+
     rolesGrpcServerHost: string
     rolesGrpcServerPort: number
+
+    kafkaBootstrapServerHost: string
+    kafkaBootstrapServerPort: number
+    kafkaBrokerUser: string
+    kafkaBrokerPassword: string
+
+    kafkaGroupConsumersCount: number
+    kafkaConsumerTopicsEvents: KafkaConsumerTopicEvent[],
 }
