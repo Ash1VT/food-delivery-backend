5744c89409dacaf053639c5d628aab7781f77d8c
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-02-24 03:33:41 +0300

    added isActive property

--- order/src/modules/restaurants/dto/restaurant.dto.ts
+++ order/src/modules/restaurants/dto/restaurant.dto.ts
@@ -1,4 +1,6 @@
-interface RestaurantBaseDto {}
+interface RestaurantBaseDto {
+    isActive: boolean
+}
 
 interface RestaurantBaseOutputDto extends RestaurantBaseDto {
     id: string
@@ -8,6 +10,11 @@ export interface RestaurantGetOutputDto extends RestaurantBaseOutputDto {}
 
 export interface RestaurantCreateInputDto extends RestaurantBaseDto {
     id: bigint
+    restaurantManagerId: bigint
 }
 
 export interface RestaurantCreateOutputDto extends RestaurantBaseOutputDto {}
+
+export interface RestaurantUpdateInputDto extends RestaurantBaseDto {}
+
+export interface RestaurantUpdateOutputDto extends RestaurantBaseOutputDto {}
--- order/src/modules/restaurants/models/restaurant.models.ts
+++ order/src/modules/restaurants/models/restaurant.models.ts
@@ -1,11 +1,14 @@
 export type RestaurantModel = {
     id: bigint
+    isActive: boolean
 }
 
 export type RestaurantCreateInput = {
     id: bigint
+    isActive: boolean
 }
 
 export type RestaurantUpdateInput = {
     id?: bigint
+    isActive?: boolean
 }
