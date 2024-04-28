9ae056fbcfbffbf39be91edadacd54703d664a55
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-04-29 01:13:00 +0300

    removed custom base

--- review/src/db/sqlalchemy/models/base.py
+++ review/src/db/sqlalchemy/models/base.py
@@ -1,14 +1,7 @@
-from sqlalchemy import Column, Integer
 from sqlalchemy.orm import declarative_base
 
 __all__ = [
-    "Base",
-    "CustomBase",
+    "Base"
 ]
 
 Base = declarative_base()
-
-
-class CustomBase(Base):
-    __abstract__ = True
-    id = Column(Integer, primary_key=True)
