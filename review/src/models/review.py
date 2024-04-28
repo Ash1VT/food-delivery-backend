4d26084621c97f2abaa15cf770a6393834ccafec
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-04-29 00:56:00 +0300

    added microservice models

---
+++ review/src/models/review.py
@@ -0,0 +1,48 @@
+from abc import ABC
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class ReviewBaseModel(ABC):
+    """
+    Base model for a review.
+    """
+
+    rating: int
+    comment: str
+
+
+@dataclass
+class ReviewModel(ReviewBaseModel):
+    """
+    Model for a review.
+    """
+
+    id: int
+    customer_id: Optional[int]
+    courier_id: Optional[int]
+    restaurant_id: Optional[int]
+    menu_item_id: Optional[int]
+    created_at: str
+
+
+@dataclass
+class ReviewCreateModel(ReviewBaseModel):
+    """
+    Model for creating a review.
+    """
+
+    customer_id: Optional[int]
+    courier_id: Optional[int]
+    restaurant_id: Optional[int]
+    menu_item_id: Optional[int]
+
+
+@dataclass
+class ReviewUpdateModel(ReviewBaseModel):
+    """
+    Model for updating a review.
+    """
+
+    pass
