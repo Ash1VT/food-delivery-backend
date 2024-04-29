c25508f7973788ca9cbc700d61977a1c61225d15
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-04-30 00:52:36 +0300

    changed for tests

--- review/src/main.py
+++ review/src/main.py
@@ -1,3 +1,11 @@
+from dataclasses import asdict
+
+from sqlalchemy import insert
+
+from db.sqlalchemy.models import Customer
+from db.sqlalchemy.session import get_async_session, async_session_maker
+from models.customer import CustomerCreateModel, CustomerUpdateModel
+from repositories.sqlalchemy.customer import CustomerRepository
 from setup import app, start_app
 from config import get_settings
 from setup.logger import logger
@@ -5,7 +13,12 @@ from setup.logger import logger
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    session = async_session_maker()
+    repository = CustomerRepository(session)
+
+    customer = await repository.retrieve(1)
+    await session.close()
+    return {"customer": customer}
 
 
 if __name__ == "__main__":
