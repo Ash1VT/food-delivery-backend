ac42db8247cf5c302cf1bdb4e292075c6d9b9e2d
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-05-02 20:12:50 +0300

    removed grpc roles

--- review/src/roles.py
+++ review/src/roles.py
@@ -6,15 +6,6 @@ class UserRole(ABC):
     Abstract base class for a user role.
     """
 
-    # @property
-    # @abstractmethod
-    # def grpc_role(self):
-    #     """
-    #     Returns the gRPC representation of the role.
-    #     """
-    #
-    #     raise NotImplementedError()
-
     @abstractmethod
     def __str__(self):
         raise NotImplementedError()
@@ -25,10 +16,6 @@ class CustomerRole(UserRole):
     Role for a customer.
     """
 
-    # @property
-    # def grpc_role(self):
-    #     return GrpcUserRole.RESTAURANT_MANAGER
-
     def __str__(self):
         return "Customer"
 
@@ -37,10 +24,6 @@ class CourierRole(UserRole):
     """
     Role for a courier.
     """
-
-    # @property
-    # def grpc_role(self):
-    #     return GrpcUserRole.MODERATOR
 
     def __str__(self):
         return "Courier"
