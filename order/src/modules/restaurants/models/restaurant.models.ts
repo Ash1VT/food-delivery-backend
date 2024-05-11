0b4f988f8039a8ae9ebec6eb69609b6514304ecc
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-05-11 17:20:52 +0300

    updated restaurant model

--- order/src/modules/restaurants/models/restaurant.models.ts
+++ order/src/modules/restaurants/models/restaurant.models.ts
@@ -1,14 +1,17 @@
 export type RestaurantModel = {
     id: bigint
+    address: string
     isActive: boolean
 }
 
 export type RestaurantCreateInput = {
     id: bigint
+    address: string
     isActive: boolean
 }
 
 export type RestaurantUpdateInput = {
     id?: bigint
+    address?: string
     isActive?: boolean
 }
