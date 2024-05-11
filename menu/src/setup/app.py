ffc82a6d38296046349e5e8ba9a459fbfd1f051d
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-05-11 16:53:18 +0300

    added initialization

--- menu/src/setup/app.py
+++ menu/src/setup/app.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from api import api_router
+from config import get_settings
 
-from consumer import init_kafka_receivers
+from consumer import consumer_creator
+from setup.kafka.consumer import init_kafka_receivers
+from setup.kafka.producer import init_producer_events
 
 # App initialization #
 
@@ -18,6 +21,29 @@ app.include_router(api_router)
 
 @app.on_event("startup")
 def start_kafka_receivers():
-    kafka_receivers = init_kafka_receivers()
-    for kafka_receiver in kafka_receivers:
-        kafka_receiver.start_receiving()
+    try:
+        settings = get_settings()
+        kafka_receivers = init_kafka_receivers(consumer_creator, settings)
+        for kafka_receiver in kafka_receivers:
+            kafka_receiver.start_receiving()
+    except Exception as e:
+        print(e)
+
+
+@app.on_event("startup")
+def init_kafka_producer_events():
+    try:
+        settings = get_settings()
+        init_producer_events(settings)
+    except Exception as e:
+        print(e)
+
+
+@app.on_event("startup")
+def authenticate_firebase():
+    try:
+        from setup.firebase import init_firebase
+        settings = get_settings()
+        init_firebase(settings)
+    except Exception as e:
+        print(e)
