899c96e6126e2e44fa1127f7b0cc11067e5e4bbf
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-05-11 17:43:21 +0300

    registered customers and addresses

--- order/src/core/routes/api.routes.ts
+++ order/src/core/routes/api.routes.ts
@@ -1,6 +1,8 @@
+import { addressRouter } from "@src/modules/addresses/routes/address.routes";
 import { orderRouter } from "@src/modules/orders/routes/order.routes";
 import { promocodeRouter } from "@src/modules/promotions/routes/promocode.routes";
 import { restaurantRouter } from "@src/modules/restaurants/routes/restaurant.routes";
+import { customerRouter } from "@src/modules/users/routes/customer.routes";
 import { Router } from "express";
 
 export const apiRouter = Router()
@@ -10,3 +12,7 @@ apiRouter.use("/orders", orderRouter)
 apiRouter.use("/promocodes", promocodeRouter)
 
 apiRouter.use("/restaurants", restaurantRouter)
+
+apiRouter.use("/customers", customerRouter)
+
+apiRouter.use("/addresses", addressRouter)
