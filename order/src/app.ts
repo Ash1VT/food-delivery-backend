b5e5c6aa0e3fb7cf24adacfed18ab9ec63fa42ce
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-05-11 17:13:50 +0300

    added initialization

--- order/src/app.ts
+++ order/src/app.ts
@@ -4,14 +4,16 @@ import { registerSwagger } from "./core/setup/swagger";
 import { registerAppRoutes } from "./core/setup/routes";
 import { registerErrorResponders } from "./core/setup/responders";
 import { registerRequestParsers } from "./core/setup/parsers";
-import { runKafkaReceivers } from "./core/setup/kafka";
+import { runKafkaReceivers } from "./core/setup/kafka/receiver";
+import { initProducerEventsTopics } from "./core/setup/kafka/publisher";
 
 const app = getExpressApp()
 const appSettings = getSettings()
 
 async function main() {
     await runKafkaReceivers(appSettings)
-
+    initProducerEventsTopics(appSettings)
+    
     registerRequestParsers(app)
     registerAppRoutes(app)
     registerSwagger(app)
