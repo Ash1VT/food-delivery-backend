bd8e07921a412594469d2e23734ba7ddc1a1e29a
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-05-11 16:53:00 +0300

    moved initialization functions to setup

--- menu/src/consumer/__init__.py
+++ menu/src/consumer/__init__.py
@@ -17,23 +17,3 @@ consumer_creator = KafkaConsumerSASLCreator(
     sasl_plain_username=settings.kafka_broker_user,
     sasl_plain_password=settings.kafka_broker_password
 )
-
-
-# Init kafka receivers
-
-def init_kafka_receivers() -> List[KafkaReceiver]:
-    kafka_receivers = list()
-
-    for topic, consumer_str_events in settings.kafka_consumer_topic_events.items():
-        group_id = f"{topic}_group"
-
-        # Create group of consumers
-        consumers = [consumer_creator.create(topic, str(group_id)) for _ in range(settings.kafka_group_consumers_count)]
-
-        # Transform string events to consumer events objects
-        topic_consumer_events = [import_string(str_event) for str_event in consumer_str_events]
-
-        # Add group of consumers to kafka receivers
-        kafka_receivers.extend((KafkaReceiver(consumer, topic_consumer_events) for consumer in consumers))
-
-    return kafka_receivers
