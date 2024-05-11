e2b4c2e1c15f9c2c5de312b6dcb2a2077e1baa24
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-05-11 16:36:46 +0300

    added dummy publisher

--- user-management/src/producer/publisher.py
+++ user-management/src/producer/publisher.py
@@ -1,18 +1,37 @@
 import logging
+from abc import ABC, abstractmethod
 
 from kafka import KafkaProducer
 
 from .events import ProducerEvent
 
 __all__ = [
-    'KafkaPublisher'
+    'AbstractPublisher',
+    'KafkaPublisher',
+    'DummyPublisher'
 ]
-
 
 logger = logging.getLogger(__name__)
 
 
-class KafkaPublisher:
+class AbstractPublisher(ABC):
+    """
+    Abstract class for publishing events to Kafka.
+    """
+
+    @abstractmethod
+    def publish(self, event: ProducerEvent):
+        """
+        Publishes event to Kafka.
+
+        Args:
+            event (ProducerEvent): The event to publish.
+        """
+
+        raise NotImplementedError
+
+
+class KafkaPublisher(AbstractPublisher):
     """
     Class for publishing events to Kafka.
     """
@@ -37,4 +56,21 @@ class KafkaPublisher:
 
         for topic in event.get_topics():
             self._producer.send(topic, key=event.get_event_name(), value=event.get_data(topic))
+            logger.info(f"Published event {event.get_event_name()} to topic: {topic}")
+
+
+class DummyPublisher(AbstractPublisher):
+    """
+    Class for publishing events to no-op.
+    """
+
+    def publish(self, event: ProducerEvent):
+        """
+        Publishes event to no-op.
+
+        Args:
+            event (ProducerEvent): The event to publish.
+        """
+
+        for topic in event.get_topics():
             logger.info(f"Published event {event.get_event_name()} to topic: {topic}")
