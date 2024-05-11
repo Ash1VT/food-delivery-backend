ffd6cc12550461fd2cb8c2525746e49c7fd19c99
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-05-11 16:50:54 +0300

    added dummy publisher

--- menu/src/producer/publisher.py
+++ menu/src/producer/publisher.py
@@ -1,13 +1,34 @@
+from abc import ABC, abstractmethod
+
 from kafka import KafkaProducer
 
 from .events import ProducerEvent
 
 __all__ = [
-    'KafkaPublisher'
+    'AbstractPublisher',
+    'KafkaPublisher',
+    'DummyPublisher',
 ]
 
 
-class KafkaPublisher:
+class AbstractPublisher(ABC):
+    """
+    Abstract class for publishing events to Kafka.
+    """
+
+    @abstractmethod
+    def publish(self, event: ProducerEvent):
+        """
+        Publishes event to Kafka.
+
+        Args:
+            event (ProducerEvent): The event to publish.
+        """
+
+        raise NotImplementedError
+
+
+class KafkaPublisher(AbstractPublisher):
     """
     Class for publishing events to Kafka.
     """
@@ -32,4 +53,21 @@ class KafkaPublisher:
 
         for topic in event.get_topics():
             self._producer.send(topic, key=event.get_event_name(), value=event.get_data(topic))
+            print(f"Published event {event.get_event_name()} to topic: {topic}")
+
+
+class DummyPublisher(AbstractPublisher):
+    """
+    Class for publishing events to no-op.
+    """
+
+    def publish(self, event: ProducerEvent):
+        """
+        Publishes event to no-op.
+
+        Args:
+            event (ProducerEvent): The event to publish.
+        """
+
+        for topic in event.get_topics():
             print(f"Published event {event.get_event_name()} to topic: {topic}")
