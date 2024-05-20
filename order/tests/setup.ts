088b5884e2a1063069ecf6c09015679e8241ff4a
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-05-20 16:48:44 +0300

    removed tests

--- order/tests/setup.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client"
-import { clearPostgres } from "./utils/prisma"
-
-afterAll(async () => {
-    const prismaClient = new PrismaClient()
-    await clearPostgres(prismaClient)
-    await prismaClient.$disconnect()
-}, 20000)
--- order/tests/utils/service.ts
+++
@@ -1,8 +0,0 @@
-import IBaseService from "@src/core/services/IBaseService";
-
-export const resetService = async (service: IBaseService) => {
-    service.customer = undefined
-    service.courier = undefined
-    service.moderator = undefined
-    service.restaurantManager = undefined
-}
