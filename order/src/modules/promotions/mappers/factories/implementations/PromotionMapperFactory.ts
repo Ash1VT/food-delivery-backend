171098efc948e5357683bcc24869c69548d66f3f
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-05-25 16:20:56 +0300

    removed promotion

--- order/src/modules/promotions/mappers/factories/implementations/PromotionMapperFactory.ts
+++ order/src/modules/promotions/mappers/factories/implementations/PromotionMapperFactory.ts
@@ -1,7 +1,5 @@
 import { PromocodeGetMapper, PromocodeCreateMapper, PromocodeUpdateMapper } from "../../implementations/promocode.mappers";
-import { PromotionGetMapper, PromotionCreateMapper } from "../../implementations/promotion.mappers";
 import { IPromocodeGetMapper, IPromocodeCreateMapper, IPromocodeUpdateMapper } from "../../interfaces/promocode.mappers";
-import { IPromotionGetMapper, IPromotionCreateMapper } from "../../interfaces/promotion.mappers";
 import IPromotionMapperFactory from "../interfaces/IPromotionMapperFactory";
 
 export default class PromotionMapperFactory implements IPromotionMapperFactory {
@@ -17,11 +15,4 @@ export default class PromotionMapperFactory implements IPromotionMapperFactory {
         return new PromocodeUpdateMapper();
     }
 
-    public createPromotionGetMapper(): IPromotionGetMapper {
-        return new PromotionGetMapper();
-    }
-
-    public createPromotionCreateMapper(): IPromotionCreateMapper {
-        return new PromotionCreateMapper();
-    }
 }
--- order/src/modules/promotions/mappers/factories/interfaces/IPromotionMapperFactory.ts
+++ order/src/modules/promotions/mappers/factories/interfaces/IPromotionMapperFactory.ts
@@ -1,10 +1,7 @@
 import { IPromocodeGetMapper, IPromocodeCreateMapper, IPromocodeUpdateMapper } from "../../interfaces/promocode.mappers";
-import { IPromotionGetMapper, IPromotionCreateMapper } from "../../interfaces/promotion.mappers";
 
 export default interface IPromotionMapperFactory {
     createPromocodeGetMapper(): IPromocodeGetMapper;
     createPromocodeCreateMapper(): IPromocodeCreateMapper;
     createPromocodeUpdateMapper(): IPromocodeUpdateMapper;
-    createPromotionGetMapper(): IPromotionGetMapper;
-    createPromotionCreateMapper(): IPromotionCreateMapper;
 }
