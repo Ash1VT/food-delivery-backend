5d7bfa8de1ab9e08188ece6063e69d854d0c2a96
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-06-19 00:56:43 +0300

    resolved conflict

--- order/src/core/setup/cors.ts
+++ order/src/core/setup/cors.ts
@@ -4,12 +4,7 @@ import cors from 'cors';
 export function registerCors(app: Express) {
     const corsOptions: cors.CorsOptions = {
         origin: [
-<<<<<<< Updated upstream
-            'http://localhost:3000',
-            'http://192.168.0.101:3000'
-=======
             '*',
->>>>>>> Stashed changes
         ],
         credentials: true
     }
